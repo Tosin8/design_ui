06a11aa876bd4df86583b367b472e0527a859c42
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-12-19 18:19:15 +0100

    first commit

---
+++ lib/screens/shoe_ui.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ShoeUI extends StatefulWidget {
+  const ShoeUI({super.key});
+
+  @override
+  State<ShoeUI> createState() => _ShoeUIState();
+}
+
+class _ShoeUIState extends State<ShoeUI> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
