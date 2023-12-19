e3b148eaccf100e192572eb609b02ca33cefa99e
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-12-19 20:59:36 +0100

    Done with Splash Screen

--- lib/screens/shoe_ui.dart
+++ lib/screens/shoe_ui.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 
 class ShoeUI extends StatefulWidget {
@@ -11,26 +12,38 @@ class _ShoeUIState extends State<ShoeUI> {
   @override
   Widget build(BuildContext context) {
     return Scaffold( 
-      body: Container(
-        width: double.infinity,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/shoe_ui/8.jpg'),
-            fit: BoxFit.cover,
-          ),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  
-                 ), 
-                 child: Text('Shop Now'),
+      body: FadeIn(
+        duration: const Duration(seconds: 5),
+        child: Container(
+          width: double.infinity,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/shoe_ui/8.jpg'),
+              fit: BoxFit.cover,
+            ),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                FadeInUp(
+                  duration: const Duration(seconds: 8),
+                  child: Container(
+                    width: 200, height: 50,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.blueAccent, 
+                      boxShadow: [BoxShadow(color: Colors.white.withOpacity(0.8), spreadRadius: 1, blurRadius: 1,)]
+                      
+                     ), 
+                     child: const Align(child: Text('Shop Now', 
+                     style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600, color: Colors.white),)),
+                    ),
                 ),
-            ],
-          ),
-          ),
+                  const SizedBox(height: 60,)
+              ],
+            ),
+            ),
+      ),
       );
     
   }
