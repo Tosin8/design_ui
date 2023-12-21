2267e440e2da6478a1ad221ed5f70ad14782c632
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-12-21 16:58:23 +0100

    Update vanilla_home.dart
    Creating Class for Contact List

--- lib/screens/vanilla_contact/vanilla_home.dart
+++ lib/screens/vanilla_contact/vanilla_home.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 
+
+// Creating class for Contact List
+class Contact {
+  final String name;
+  const Contact({
+    required this.name, 
+  });
+}
 class VanillaHome extends StatelessWidget {
   const VanillaHome({super.key});
 
