bcaf0463fd9e6025f0cd4ff8bb1b79d44d696ebb
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-12-21 17:06:12 +0100

    Update vanilla_home.dart
    Updating with Contact Storage

--- lib/screens/vanilla_contact/vanilla_home.dart
+++ lib/screens/vanilla_contact/vanilla_home.dart
@@ -8,6 +8,16 @@ class Contact {
     required this.name, 
   });
 }
+
+// Creating a Singleton of ContactBook, which is the _sharedInstance(), _shared and factor constructor. 
+class ContactBook {
+  ContactBook._sharedInstance(); // creating a private constructor. 
+static final ContactBook _shared = ContactBook._sharedInstance(); 
+factory ContactBook() => _shared; 
+
+// CREATING A CONACTS STORAGE. 
+final List<Contact> _contacts = [];
+}
 class VanillaHome extends StatelessWidget {
   const VanillaHome({super.key});
 
