dbbe3a2b39bb0340253a74732596a7eca0bef61e
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-12-21 16:56:59 +0100

    Refactoring to Vanilla Contact

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:ui_design/screens/shoe_ui.dart';
+import 'package:ui_design/screens/vanilla_contact/vanilla_home.dart';
+
+
+import 'screens/shoe_app/shoe_ui.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,7 +22,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const ShoeUI()
+      home: const VanillaHome()
     );
   }
 }
