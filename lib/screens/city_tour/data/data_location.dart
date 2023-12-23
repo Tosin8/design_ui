9ad2ae204d610be2ba555c651404d84adc9dfca9
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-12-23 02:16:04 +0100

    Create class for data location

---
+++ lib/screens/city_tour/data/data_location.dart
@@ -0,0 +1,14 @@
+import '../model/location.dart';
+import 'data_review.dart';
+
+List<Location> locations = [
+  Location(
+    name: 'AtCoastal', 
+    urlImage: 'assets/city_tour/thailand.jpg',
+     latitude: 'North Lat 24', 
+     longitude: 'East Lng 18', 
+     addressLine1: 'La Crensenta- Montrose, Glendale', 
+     addressLine2: 'No. 1484848',
+      starRating: 4, 
+      reviews: Reviews.allReviews)
+]; 
--- lib/screens/city_tour/data/data_review.dart
+++ lib/screens/city_tour/data/data_review.dart
@@ -8,6 +8,24 @@ class Reviews {
       date: 'Feb 14th', 
       username: 'Mike Jimrose',
       urlImage: '', 
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat' ), 
+
+       Review (
+      date: 'Jan 13rd', 
+      username: 'Mike Jimrose',
+      urlImage: '', 
+      description: '' ), 
+      
+       Review (
+      date: 'Aug 28th', 
+      username: 'Mike Jimrose',
+      urlImage: '', 
+      description: '' ),  
+      
+      Review (
+      date: 'Sep 31st', 
+      username: 'Mike Jimrose',
+      urlImage: '', 
       description: '' ), 
 
   ]; 
