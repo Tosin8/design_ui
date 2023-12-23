7204a02f8d339657e02855489a574bc88b31bfba
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-12-23 02:08:42 +0100

    Creating data review

---
+++ lib/screens/city_tour/data/data_review.dart
@@ -0,0 +1,14 @@
+
+import '../model/review.dart';
+
+class Reviews {
+  static List<Review> allReviews = [ 
+
+    Review (
+      date: 'Feb 14th', 
+      username: 'Mike Jimrose',
+      urlImage: '', 
+      description: '' ), 
+
+  ]; 
+}
