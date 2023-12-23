b8e2daf3ddb78b6105f773b1bcaeb1842c8b906e
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-12-23 01:17:41 +0100

    Refactoring for City_Tour

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,15 @@
-import 'package:flutter/material.dart';
-import 'package:ui_design/screens/vanilla_contact/vanilla_home.dart';
-
-
+import 'package:flutter/material.dart'; 
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MyApp()); 
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const VanillaHome(), 
-      routes: {
-        '/new-contact':(context) => NewContactView(), 
-      },
-    );
+    return Container();
   }
 }
-
