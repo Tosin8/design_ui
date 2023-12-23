8b193f39ddcee8739b13851540881ed1e1f20bb0
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-12-23 02:36:21 +0100

    Update city_image.dart

--- lib/screens/city_tour/extension/city_image.dart
+++ lib/screens/city_tour/extension/city_image.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
+import 'city_location.dart';
+
 class CityImage extends StatelessWidget {
-  const CityImage({super.key});
+
+  final CityLocation location; 
+  const CityImage({super.key, required this.location});
 
   @override
   Widget build(BuildContext context) {
@@ -10,6 +14,27 @@ class CityImage extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 16), 
       height: size.height * 0.5, 
       width: size.width * 0.8, 
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black26, blurRadius: 2, spreadRadius: 1
+          )
+        ], 
+        borderRadius: BorderRadius.all(Radius.circular(8)), 
+      ),
+      child: Stack(
+        children: [
+          buildImage(), 
+          Container(
+            padding: const EdgeInsets.all(8), 
+            child: Column(mainAxisAlignment: MainAxisAlignment.spaceBetween, 
+            children: [
+buildTopText(), 
+LatLongWidget(location: location), 
+            ],)
+          )
+        ],
+      ),
     );
   }
 }
