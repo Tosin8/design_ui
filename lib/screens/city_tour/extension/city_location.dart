ab6813d786db721c2da907ab9dfd798b1c0e5d54
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-12-23 03:49:56 +0100

    Update city_location.dart

--- lib/screens/city_tour/extension/city_location.dart
+++ lib/screens/city_tour/extension/city_location.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
 
+import '../model/location.dart';
+
 class CityLocation extends StatefulWidget {
-  const CityLocation({super.key});
+  final Location location; 
+  const CityLocation({super.key, required this.location});
 
   @override
   State<CityLocation> createState() => _CityLocationState();
 }
 
 class _CityLocationState extends State<CityLocation> {
+  bool isExpanded = false;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final size = MediaQuery.of(context).size; 
+    return Padding(padding: const EdgeInsets.symmetric(horizontal: 4));
   }
 }
