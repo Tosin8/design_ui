7ce6cac9617a0ccfb6bd29cb805779dff4823035
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-12-23 02:21:09 +0100

    Update data_location.dart

--- lib/screens/city_tour/data/data_location.dart
+++ lib/screens/city_tour/data/data_location.dart
@@ -10,5 +10,34 @@ List<Location> locations = [
      addressLine1: 'La Crensenta- Montrose, Glendale', 
      addressLine2: 'No. 1484848',
       starRating: 4, 
-      reviews: Reviews.allReviews)
+      reviews: Reviews.allReviews), 
+
+      Location(
+    name: 'Syracuse', 
+    urlImage: 'assets/city_tour/bejing_china.jpg',
+     latitude: 'South Lat 14', 
+     longitude: 'East Lng 27', 
+     addressLine1: 'La Crensenta- Montrose, Glendale', 
+     addressLine2: 'No. 1484848',
+      starRating: 4, 
+      reviews: Reviews.allReviews),
+
+        Location(
+    name: 'Oceanic', 
+    urlImage: 'assets/city_tour/new_york.jpg',
+     latitude: 'South Lat 14', 
+     longitude: 'East Lng 27', 
+     addressLine1: 'La Crensenta- Montrose, Glendale', 
+     addressLine2: 'No. 1484848',
+      starRating: 4, 
+      reviews: Reviews.allReviews),  
+      Location(
+    name: 'Mountainous', 
+    urlImage: 'assets/city_tour/singapore.jpg',
+     latitude: 'South Lat 14', 
+     longitude: 'East Lng 27', 
+     addressLine1: 'La Crensenta- Montrose, Glendale', 
+     addressLine2: 'No. 1484848',
+      starRating: 4, 
+      reviews: Reviews.allReviews),
 ]; 
