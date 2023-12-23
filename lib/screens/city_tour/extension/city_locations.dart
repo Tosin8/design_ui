668e33b29a9e54db2b44cd274911be46cd08e9c5
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-12-23 03:46:08 +0100

    Update city_locations.dart

--- lib/screens/city_tour/extension/city_locations.dart
+++ lib/screens/city_tour/extension/city_locations.dart
@@ -1,15 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:ui_design/screens/city_tour/data/data_location.dart';
+import 'package:ui_design/screens/city_tour/model/location.dart';
 
 class CityLocations extends StatefulWidget {
-  const CityLocations({super.key});
+  const CityLocations({super.key, required Location location});
 
   @override
   State<CityLocations> createState() => _CityLocationsState();
 }
 
 class _CityLocationsState extends State<CityLocations> {
+
+  final pageController = PageController(viewportFraction: 0.8);
+  int pageIndex = 0; 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column
+    (
+      children: [
+        Expanded(
+          child: PageView.builder(
+            itemCount: locations.length,
+            itemBuilder: (context, index) {
+              final location = locations[index]; 
+              return CityLocations(location: location); 
+          
+        }, 
+        onPageChanged: (index) =>  setState(() =>
+          pageIndex = index), 
+        
+        )), 
+        Text(
+          '${pageIndex + 1} /${locations.length}', 
+          style: TextStyle(color: Colors.white70),
+        )
+      ],
+    );
   }
 }
