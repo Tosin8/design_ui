bd7709ba8c1c81616432ef46909c2d76b336f174
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-12-23 01:43:36 +0100

    Update location.dart
    Creating Class Location

---
+++ lib/screens/city_tour/model/location.dart
@@ -0,0 +1,12 @@
+class Location {
+  final String name; 
+  final String urlImage; 
+  final String latitude; 
+  final String longitude;
+  final String addressLine1; 
+  final String addressLine2;
+  final int starRating; 
+  final List<Review> reviews;
+
+  Location({required this.name, required this.urlImage, required this.latitude, required this.longitude, required this.addressLine1, required this.addressLine2, required this.starRating, required this.reviews}); 
+}
