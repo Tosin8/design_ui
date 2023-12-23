46f41263f0586fd8f73e4b532840a94b8a3b1ed2
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-12-23 03:04:30 +0100

    Create city_locations.dart

---
+++ lib/screens/city_tour/extension/city_locations.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CityLocations extends StatefulWidget {
+  const CityLocations({super.key});
+
+  @override
+  State<CityLocations> createState() => _CityLocationsState();
+}
+
+class _CityLocationsState extends State<CityLocations> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
