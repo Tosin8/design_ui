8ddcb538947ced1ea2318b769fac7b8a83311be5
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-12-26 23:39:58 +0100

    Update splash.dart

--- lib/screens/furniture_app/splash.dart
+++ lib/screens/furniture_app/splash.dart
@@ -5,51 +5,56 @@ class FurnitureSplash extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold( 
-      body: Column(
-        children: [
-          Container(
-            height: 450, width: double.infinity,
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/furniture/splash.jpg'),
-                fit: BoxFit.cover,
-              ),
-            ),
-            child: Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.bottomCenter,
-                  end: Alignment.topRight,
-                  colors: [
-                    Colors.black.withOpacity(0.3),
-                    Colors.black.withOpacity(0.0),
-                  ],
+    return SafeArea(
+      child: Scaffold( 
+        body: ListView(
+          children: [
+            Container(
+              height: 450, width: double.infinity,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/furniture/splash.jpg'),
+                  fit: BoxFit.cover,
                 ),
               ),
-            ),
-          ), 
-          Container(
-            width: double.infinity,
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topRight,
+                    colors: [
+                      Colors.black.withOpacity(0.3),
+                      Colors.black.withOpacity(0.0),
+                    ],
+                  ),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.only(top: 20, left: 180),
+                  child: Text('KTEA', style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.w500, ),
+                  )),
+              ),
+            ), 
+            Container(
+              width: double.infinity,
+              height: 330,
+               
+                 color: const Color(0xffCEC8BF),
              
-               color: const Color(0xffCEC8BF),
-           
-            child: const Stack(
-              fit: StackFit.loose,
-              clipBehavior: Clip.none,
-
-              children:[
-                
-                Positioned(
-                  top: -70,left: 20,
-                  child: Text(
-                    'Elegant\nSimple\nFurnitures.',
-                     style: TextStyle(fontSize: 40, fontWeight: FontWeight.w500),)), 
-               Positioned(
-                left: 20, top: 30, 
-                child: Text('Furniture your style \nwith teh most remarkable 24th century. ', style: TextStyle(fontWeight: FontWeight.w500, fontSize: 18)))]),
-          )
-        ],
+              child: const Stack(
+                fit: StackFit.loose,
+                clipBehavior: Clip.none,
+      
+                children:[
+                  
+                  Positioned(
+                    top: -70,left: 20,
+                    child: Text(
+                      'Elegant\nSimple\nFurnitures.',
+                       style: TextStyle(fontSize: 40, fontWeight: FontWeight.w500),)), 
+                  Text('Furniture your style \nwith teh most remarkable 24th century. ', style: TextStyle(fontWeight: FontWeight.w500, fontSize: 18))]),
+            )
+          ],
+        ),
       ),
     );
   }
