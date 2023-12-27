d51902d9a5933f18a6c4a357f45f9c4b2523837d
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-12-27 01:15:29 +0100

    Create size_config.dart

---
+++ lib/screens/furniture_app/size_config.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class SizeConfig {
+  late MediaQueryData _mediaQueryData;
+  late double screenWidth;
+  late  double screenHeight;
+  static late double defaultSize;
+  static late Orientation orientation;
+
+  void init(BuildContext context) {
+    _mediaQueryData = MediaQuery.of(context);
+    screenWidth = _mediaQueryData.size.width;
+    screenHeight = _mediaQueryData.size.height;
+    orientation = _mediaQueryData.orientation;
+    // On iPhone 11 the defaultSize = 10 almost
+    // So if the screen size increase or decrease then our defaultSize also vary
+    defaultSize = orientation == Orientation.landscape
+        ? screenHeight * 0.024
+        : screenWidth * 0.024;
+  }
+}
