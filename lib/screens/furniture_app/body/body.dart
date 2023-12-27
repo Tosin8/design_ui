8e9daf9b7dc678ec3d9b6f042915d7d72661a490
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-12-27 01:50:12 +0100

    Creating UI for Home Screen

--- lib/screens/furniture_app/body/body.dart
+++ lib/screens/furniture_app/body/body.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../constants.dart';
+import '../size_config.dart';
 
 class FurnitureBody extends StatelessWidget {
   const FurnitureBody({super.key});
@@ -6,6 +11,24 @@ class FurnitureBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false, 
+        backgroundColor: Colors.transparent,
+        elevation: 0, 
+        systemOverlayStyle: SystemUiOverlayStyle.light,
+      leading: IconButton(onPressed: (){},
+       icon: SvgPicture.asset(
+        'assets/furniture/icons/menu.svg', 
+        height: SizeConfig.defaultSize * 2,)),
+       actions: [
+        IconButton(onPressed: (){},
+        icon: SvgPicture.asset('assets/furniture/icons/search.svg')),
+        Center(
+          child: Text('Scan', style: TextStyle(color: kTextColor, fontWeight: FontWeight.bold),)
+        )
+       
+       ],
+      ),
       body: Container(
         child: Text('Hello')
       ),
