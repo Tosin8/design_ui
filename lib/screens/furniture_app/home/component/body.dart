82bd4c71a9a0476b9b22b57bb9f9763d8bb1367c
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-12-27 03:41:24 +0100

    Update body.dart

--- lib/screens/furniture_app/home/component/body.dart
+++ lib/screens/furniture_app/home/component/body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ui_design/screens/furniture_app/constants.dart';
 import 'package:ui_design/screens/furniture_app/model/cateogory.dart';
+import 'package:ui_design/screens/furniture_app/services/fetch_categories.dart';
 
 import 'category_card.dart';
 import 'titleText.dart';
@@ -12,24 +13,38 @@ class FurnitureBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
+    return const SingleChildScrollView(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           titleText(name: 'Browse by Categories',), 
           SizedBox(height: 20), 
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: [
-                CategoryCard(category: category,),
-                CategoryCard(category: category,),
-              ],
-            ),
-        )
+          FutureBuilder(
+            future: fetchCategories(),
+            builder: (context, snapshot) => snapshot.hasData? Categories() : CircularProgressIndicator(),
+          )
         
    ]  ) ); 
     
+  }
+}
+
+class Categories extends StatelessWidget {
+  const Categories({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: [
+          CategoryCard(category: category,),
+          CategoryCard(category: category,),
+        ],
+      ),
+            );
   }
 }
 
