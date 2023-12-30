34a8793508c640cba5313270a5924f69b33fb8a1
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-12-30 14:10:33 +0100

    Refactoring Ui for Swipe Onboard

--- lib/screens/swipe_onboard/swipe_home.dart
+++ lib/screens/swipe_onboard/swipe_home.dart
@@ -11,6 +11,12 @@ class HomeSwipe extends StatefulWidget {
 class _HomeSwipeState extends State<HomeSwipe> {
   @override
   Widget build(BuildContext context) {
+    int page = 0; 
+    final pages = [
+      
+    ]; 
     return const Placeholder();
   }
 }
+
+
