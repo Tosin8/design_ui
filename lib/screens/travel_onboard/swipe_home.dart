6f71999187a70cdb7e5410389ff8f49051f34cb1
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-12-31 18:34:26 +0100

    Update swipe_home.dart

--- lib/screens/travel_onboard/swipe_home.dart
+++ lib/screens/travel_onboard/swipe_home.dart
@@ -18,7 +18,26 @@ class _HomeSwipeState extends State<HomeSwipe> {
     ]; 
     return PageView(
       children: [
-
+Container(
+decoration: BoxDecoration(
+  color: Color(0xfffd9b42), 
+  ),
+  child: Column(
+    children: [
+      Text('Hooliday'), 
+    ],
+  )
+), 
+Container(
+decoration: BoxDecoration(
+  color: Colors.blue, 
+  ),
+), 
+Container(
+decoration: BoxDecoration(
+  color: Colors.green, 
+  ),
+), 
       ], 
     );
   }
