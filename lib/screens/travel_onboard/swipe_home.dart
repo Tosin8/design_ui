0594d5408efd4b19afc8b6d6d2fa759ff07a5dab
Tosin Ezekiel <tosinezekiel8@gmail.com>
2023-12-31 18:58:23 +0100

    Update swipe_home.dart

--- lib/screens/travel_onboard/swipe_home.dart
+++ lib/screens/travel_onboard/swipe_home.dart
@@ -22,31 +22,61 @@ class _HomeSwipeState extends State<HomeSwipe> {
         body: PageView(
           children: [
         Container(
-        decoration: BoxDecoration(
+        decoration: const BoxDecoration(
           color: Color(0xfffd9b42), 
           ),
           child: Padding(
             padding: const EdgeInsets.symmetric(vertical: 30, ), 
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-            Text('Hooliday',
-             style: TextStyle(fontSize: 20),),
-             SizedBox(height: 40,), 
-             SimpleShadow(
-               child: Image.asset('assets/travel_ui/travel.png', 
+            const Padding(
+              padding: EdgeInsets.only(left: 150,),
+              child: Text('Hooliday',
+               style: TextStyle(fontSize: 20),),
+            ),
+             const SizedBox(height: 40,), 
+        
+               SimpleShadow(
+                 child: Image.asset('assets/travel_ui/travel.png', 
+                 ),
                ),
+               const SizedBox(height: 30), 
+             const Padding(
+               padding: EdgeInsets.only(left: 25.0),
+               child: Text('We will \ntake care', style: TextStyle(fontSize: 40, 
+               fontWeight: FontWeight.w600),),
+             ),
+             const SizedBox(height: 20), 
+             const Padding(
+               padding: EdgeInsets.only(left: 25.0),
+               child: Text('of tickets, transfers and a cool\nplace to stay.', 
+               style: TextStyle(fontSize: 20),),
              ),  
+             const SizedBox(height: 150), 
+              Padding(
+               padding: EdgeInsets.only(left: 25.0),
+               child: Row(
+                children: [
+                  GestureDetector(
+                    onTap: (){},
+                    child: Text('Skip'))
+                ],
+               ),
+             )
                   ],
             ),
-          )
+          ), 
+          
         ), 
         Container(
-        decoration: BoxDecoration(
+        decoration: const BoxDecoration(
           color: Colors.blue, 
           ),
         ), 
         Container(
-        decoration: BoxDecoration(
+        decoration: const BoxDecoration(
           color: Colors.green, 
           ),
         ), 
