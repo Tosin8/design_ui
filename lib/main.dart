7caa314064e42c235c09abdab8deef86bab29f14
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-01-03 13:31:15 +0100

    Refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:ui_design/screens/custom_paint.dart';
 
 import 'screens/furniture_app/splash.dart';
 import 'screens/travel_onboard/swipe_home.dart'; 
 
 void main() {
-  runApp(const MyApp()); 
+  runApp( MyApp()); 
 }
 
 
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp( debugShowCheckedModeBanner: false, 
+//     title: 'Flutter Demo',
+//     theme: ThemeData(
+//       primarySwatch: Colors.blue,
+//     ),
+//     home: const HelloP(), 
+
+//     ); 
+//   }
+// }
+
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  var count = 0.obs; 
+  //const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp( debugShowCheckedModeBanner: false, 
-    title: 'Flutter Demo',
-    theme: ThemeData(
-      primarySwatch: Colors.blue,
-    ),
-    home: const HelloP(), 
 
-    ); 
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Counter')
+      ),
+      body: Center(
+        child: Obx(() => Text('$count')),
+      ), 
+      floatingActionButton: FloatingActionButton(onPressed: ()=> count ++, child: Icon(Icons.add),),
+    );
   }
 }
