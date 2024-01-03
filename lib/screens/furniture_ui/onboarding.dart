bbd473b8922bb4fa4aab633e0a3574e0d66c355b
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-01-03 15:27:41 +0100

    Update onboarding.dart

--- lib/screens/furniture_ui/onboarding.dart
+++ lib/screens/furniture_ui/onboarding.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 
 class FurnitureOnboarding extends StatefulWidget {
@@ -13,18 +14,18 @@ class _FurnitureOnboardingState extends State<FurnitureOnboarding> with SingleTi
 
     {
       'title': 'Modern \nFurnitures', 
-      'sub_title': 'Choose from thousands of items that fits your choice', 
+      'sub_title': 'Choose from thousands\nof items that fits your choice', 
       'image': 'assets/furniture_onboard/1.jpg', 
     }, 
 
     {
       'title': 'Modern \nFurnitures', 
-      'sub_title': 'Choose from thousands of items that fits your choice', 
+      'sub_title': 'Choose from thousands\nof items that fits your choice', 
       'image': 'assets/furniture_onboard/2.jpg', 
     }, 
     {
       'title': 'Modern \nFurnitures', 
-      'sub_title': 'Choose from thousands of items that fits your choice', 
+      'sub_title': 'Choose from thousands\nof items that fits your choice', 
       'image': 'assets/furniture_onboard/3.jpg', 
     }
   ]; 
@@ -54,6 +55,9 @@ late final Animation<double> _animation = Tween<double> (
           _controller.value = 0.0; 
           _controller.forward(); 
         },
+
+        itemCount: _furnitures.length,
+        controller: PageController(viewportFraction: 1.0),
         itemBuilder: (context, index) {
           return Container(
 child: Stack (
@@ -70,7 +74,74 @@ child: Stack (
       fit: BoxFit.cover, 
       ),),
     ), 
-    Positioned(child: Container())
+    Positioned(
+      bottom: 0, 
+
+      child: Container(
+        padding: const EdgeInsets.all(20), 
+        width: MediaQuery.of(context).size.width, 
+        height: MediaQuery.of(context).size.height, 
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.bottomRight, 
+            colors: [
+              Colors.black.withOpacity(0.9), 
+              Colors.black.withOpacity(0.8), 
+              Colors.black.withOpacity(0.2), 
+              Colors.black.withOpacity(0.1), 
+            ])
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            FadeInDown(
+              duration: const Duration(milliseconds: 500),
+              child: Text(_furnitures[index]['title'], 
+              style: const TextStyle(
+                color: Colors.white ,
+                fontSize: 42, 
+                fontWeight: FontWeight.bold),
+                ),
+            ),
+            const SizedBox(height: 10), 
+            FadeInLeft(
+              delay: const Duration(milliseconds: 100), 
+              duration: const Duration(milliseconds: 800),
+              child: Text(_furnitures[index]['sub_title'], 
+              style: TextStyle(color: Colors.grey.shade400, fontSize: 18),),
+            ), 
+            const SizedBox(height: 50), 
+            FadeInRight(
+              delay: const Duration(milliseconds: 100), 
+              duration: const Duration(milliseconds: 1000),
+              child: Align(
+                alignment: Alignment.bottomRight, 
+                child: MaterialButton(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(40)
+                  ), 
+                  
+                  onPressed: (){}, 
+                  color: Colors.orange, 
+                  padding: const EdgeInsets.only(right: 5, left: 30, top: 5, bottom: 5), child: Container(height: 40, width: MediaQuery.of(context).size.width * 0.4, 
+                  child: Row(
+                    children: [
+                      const Text('Get Started', 
+                      style: TextStyle(color: Colors.black, fontSize: 16, 
+                      ),), 
+                      const Spacer(), 
+                      Container(
+                        padding: const EdgeInsets.all(8), 
+                        decoration: BoxDecoration(color: Colors.orange.shade300, borderRadius: BorderRadius.circular(40)),child: Icon(Icons.arrow_forward_ios, color: Colors.orange.shade100,),
+                      )
+                    ],
+                  ),),),
+              )), 
+              SizedBox(height: 20,)
+          ],
+        ),
+      ))
   ],)
           ); 
         })
