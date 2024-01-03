d72c90946036bd6dcb9adfca5d1e143b0a69782c
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-01-03 14:44:18 +0100

    Update onboarding.dart
    Updating with List

--- lib/screens/furniture_ui/onboarding.dart
+++ lib/screens/furniture_ui/onboarding.dart
@@ -1,8 +1,33 @@
 import 'package:flutter/material.dart';
 
-class FurnitureOnboarding extends StatelessWidget {
+class FurnitureOnboarding extends StatefulWidget {
   const FurnitureOnboarding({super.key});
 
+  @override
+  State<FurnitureOnboarding> createState() => _FurnitureOnboardingState();
+}
+
+class _FurnitureOnboardingState extends State<FurnitureOnboarding> with SingleTickerProviderStateMixin{
+
+  final List<dynamic> _furnitures = [
+
+    {
+      'title': 'Modern \nFurnitures', 
+      'sub_title': 'Choose from thousands of items that fits your choice', 
+      'image': 'assets/furniture_onboard/1.jpg', 
+    }, 
+
+    {
+      'title': 'Modern \nFurnitures', 
+      'sub_title': 'Choose from thousands of items that fits your choice', 
+      'image': 'assets/furniture_onboard/2.jpg', 
+    }, 
+    {
+      'title': 'Modern \nFurnitures', 
+      'sub_title': 'Choose from thousands of items that fits your choice', 
+      'image': 'assets/furniture_onboard/3.jpg', 
+    }
+  ]; 
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
