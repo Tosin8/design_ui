a148004a996200897235ec0ec564787be209435c
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-01-04 23:55:46 +0100

    done with appbar

--- lib/screens/furniture_gridview/gridview.dart
+++ lib/screens/furniture_gridview/gridview.dart
@@ -1,4 +1,7 @@
+import 'package:badges/badges.dart' as badges;
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
 
 class FurnitureGrid extends StatefulWidget {
   const FurnitureGrid({super.key});
@@ -12,13 +15,29 @@ class _FurnitureGridState extends State<FurnitureGrid> {
   Widget build(BuildContext context) {
     return Scaffold( 
       appBar: AppBar(
-        leading: Icon(Icons.menu), 
-        title: Text('Furniture'),
+        leading: const Icon(Icons.menu), 
+        title: const Text('Furniture'),
+        centerTitle: true,
         actions: [
-          
-        ],
+badges.Badge(
+  badgeContent: const Text('0', style: TextStyle(color: Colors.white),), 
+  position: badges.BadgePosition.topEnd(top: -5, end: -4 ),
+  badgeStyle: badges.BadgeStyle(
+    badgeColor: Colors.black, 
+  ),
+  
+  //backgroundColor: Colors.blue,
+  child: IconButton(onPressed: (){}, 
+  icon: const Icon(FontAwesomeIcons.basketShopping),
+         ),
+         
+), 
+SizedBox(width: 10), 
+ ],
+ 
       ),
-      body: Text('Hello World')
+
+      body: const Text('Hello World')
     );
   }
 }
