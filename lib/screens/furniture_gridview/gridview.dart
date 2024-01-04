334d14c5f4595e7723d23f380704c963547fdbc9
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-01-05 00:10:52 +0100

    Update gridview.dart

--- lib/screens/furniture_gridview/gridview.dart
+++ lib/screens/furniture_gridview/gridview.dart
@@ -22,7 +22,7 @@ class _FurnitureGridState extends State<FurnitureGrid> {
 badges.Badge(
   badgeContent: const Text('0', style: TextStyle(color: Colors.white),), 
   position: badges.BadgePosition.topEnd(top: -5, end: -4 ),
-  badgeStyle: badges.BadgeStyle(
+  badgeStyle: const badges.BadgeStyle(
     badgeColor: Colors.black, 
   ),
   
@@ -32,22 +32,52 @@ badges.Badge(
          ),
          
 ), 
-SizedBox(width: 10), 
+const SizedBox(width: 10), 
  ],
  
       ),
 
-      body: Column(
-        children: [
-          Container(
-            
-            decoration: BoxDecoration(
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(8.0), 
+        child: Column(
+          children: [
+            Container(
+              height: 250,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                
+                image: const DecorationImage(image: AssetImage('assets/furniture_grid/9.jpg',), fit: BoxFit.cover 
               
-              image: DecorationImage(image: AssetImage('assets/furniture_grid/9.jpg'), 
-            
-            )),
-          ),
-        ],
+              )),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  gradient: LinearGradient(colors: [
+                    Colors.black.withOpacity(.4), 
+                    Colors.black.withOpacity(.1),
+                     
+                  ], begin: Alignment.bottomCenter)
+                ),
+                child:  Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    const Text('Lifestyle Sales', style: TextStyle(color: Colors.white, fontWeight: FontWeight.w600, fontSize: 30),
+                    ), 
+                    const SizedBox(height: 20), 
+                    Container(
+                      height: 50,
+                      margin: const EdgeInsets.symmetric(horizontal: 50),
+                      decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(10)),
+                    ), 
+                    const SizedBox(height: 20), 
+
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       )
     );
   }
