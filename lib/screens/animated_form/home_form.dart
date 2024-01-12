fc5d96e476b74b69ac7f6889be74ce41277fa26a
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-01-12 16:13:12 +0100

    Update home_form.dart

--- lib/screens/animated_form/home_form.dart
+++ lib/screens/animated_form/home_form.dart
@@ -246,3 +246,12 @@ Widget component1(IconData icon, String hintText, bool isPassword, bool isEmail)
        fontSize: 14),
     ),),)));
 }
+
+Widget component2(String string, double Width, VoidCallback voidCallback) {
+  Size size = MediaQuery.of(context).size; 
+  return ClipRRect(
+    borderRadius: BorderRadius.circular(15), child: BackdropFilter(filter: ImageFilter.blur(sigmaX: 15, sigmaY: 15),child: InkWell(highlightColor: Colors.transparent, splashColor: Colors.transparent, onTap: voidCallback, 
+    child: Container(height: size.width / 8, width: size.width / width, alignment: Alignment.center, decoration: BoxDecoration(color: Colors.white.withOpacity(0.05), borderRadius: BorderRadius.circular(15), ), child: Text(string, style: TextStyle(color: Colors.white.withOpacity(.8)),
+    ),),),),
+  );
+}
