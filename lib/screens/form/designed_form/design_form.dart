3ab7ec2ed07df08c28d9c705acefeec0b5935088
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-01-12 17:30:44 +0100

    Update design_form.dart

--- lib/screens/form/designed_form/design_form.dart
+++ lib/screens/form/designed_form/design_form.dart
@@ -20,42 +20,57 @@ class _DesignFormState extends State<DesignForm> {
         ),
         child: Container(
           
-        child:  Padding( 
-          padding: EdgeInsets.only(left: 10),
+        child:  const Padding( 
+          padding: EdgeInsets.only(left: 20, right: 20),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.end,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               
-              Text("PLANT'D", style: TextStyle(fontSize: 28, fontWeight: FontWeight.w600, color: Colors.white, letterSpacing: 1, ),),
+              Text("PLANT'D", 
+              style: TextStyle(fontSize: 28, fontWeight: FontWeight.w600, color: Colors.white, letterSpacing: 1, ),),
               SizedBox(height: 20), 
           Text('Let\'s get your plant wet',style: TextStyle(color: Colors.white, fontSize: 20),
           ), 
           SizedBox(height: 20), 
-          Form(child: Column(
-            children: [
-              Container(
-                 height: 50,
-                decoration: BoxDecoration(
-                   color: Colors.white,
-                ),
-               
-               
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: 'Enter Email', hintStyle: TextStyle(color: Colors.black),
-                    prefixIcon: Icon(Icons.email_outlined,)
-                  )
-                ),
-              )
-            ],
-          ))
+          SignInForm()
             ],
           ),
         ),
         ),
       )
     );
+  }
+}
+
+class SignInForm extends StatelessWidget {
+  const SignInForm({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(child: Column(
+      children: [
+        Container(
+           height: 55,
+          decoration:  BoxDecoration(
+             color: Colors.grey.shade200,
+             borderRadius: BorderRadius.circular(10), 
+          ),
+         
+         
+          child: const Align(
+            child: TextField(
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: 'Enter Email', hintStyle: TextStyle(color: Colors.black),
+                prefixIcon: Icon(Icons.email_outlined,)
+              )
+            ),
+          ),
+        )
+      ],
+    ));
   }
 }
