8224cf7a29ca8bff0c6b693276300cf83f620a8b
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-01-12 18:32:54 +0100

    Update design_form.dart

--- lib/screens/form/designed_form/design_form.dart
+++ lib/screens/form/designed_form/design_form.dart
@@ -12,91 +12,29 @@ class _DesignFormState extends State<DesignForm> {
   @override
   Widget build(BuildContext context) {
     return Scaffold( 
-      body: SingleChildScrollView(
-        child: Container(
-          width: double.infinity, 
-          height: double.infinity,
-          decoration: BoxDecoration(
-            image: DecorationImage(image: bkg, fit: BoxFit.cover)
-          ),
-          child: Container(
-            
-          child:  const Padding( 
-            padding: EdgeInsets.only(left: 20, right: 20),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                
-                Text("PLANT'D", 
-                style: TextStyle(fontSize: 28, fontWeight: FontWeight.w600, color: Colors.white, letterSpacing: 1, ),),
-                SizedBox(height: 20), 
-            Text('Let\'s get your plant wet',style: TextStyle(color: Colors.white, fontSize: 20),
-            ), 
-            SizedBox(height: 20), 
-            SignInForm()
-              ],
-            ),
-          ),
-          ),
+      body: Container(
+        width: double.infinity, 
+        height: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(image: bkg, fit: BoxFit.cover)
         ),
-      )
-    );
-  }
-}
-
-class SignInForm extends StatelessWidget {
-  const SignInForm({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Form(child: Column(
-      children: [
-        Container(
-           height: 55,
-          decoration:  BoxDecoration(
-             color: Colors.grey.shade200,
-             borderRadius: BorderRadius.circular(10), 
-          ),
-         
-         
-          child: const Align(
-            child: TextField(
-              textInputAction: TextInputAction.next,
-              decoration: InputDecoration(
-                border: InputBorder.none,
-                hintText: 'Enter Email', hintStyle: TextStyle(color: Colors.black),
-                prefixIcon: Icon(Icons.email_outlined,)
-              )
-            ),
-          ),
-        ), 
-        const SizedBox(height: 20), 
-        Container(
-           height: 55,
-          decoration:  BoxDecoration(
-             color: Colors.grey.shade200,
-             borderRadius: BorderRadius.circular(10), 
-          ),
-         
-         
-          child: const Align(
-            child: TextField(
-              obscureText: true,
-              textInputAction: TextInputAction.done,
-              decoration: InputDecoration(
-                floatingLabelBehavior: FloatingLabelBehavior.always,
-                border: InputBorder.none,
-                
-                hintText: 'Enter Password', hintStyle: TextStyle(color: Colors.black),
-                prefixIcon: Icon(Icons.key,)
-              )
-            ),
-          ),
-        ), 
-        const SizedBox(height: 20), 
+        child: Container(
+          
+        child: Padding( 
+          padding: const EdgeInsets.only(left: 20, right: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              
+              const Text("PLANT'D", 
+              style: TextStyle(fontSize: 28, fontWeight: FontWeight.w600, color: Colors.white, letterSpacing: 1, ),),
+              const SizedBox(height: 20), 
+          const Text('Let\'s get your plant wet',style: TextStyle(color: Colors.white, fontSize: 20),
+          ), 
+          const SizedBox(height: 20), 
+          const SignInForm(), 
+          const SizedBox(height: 20), 
         Row(
           children: [
             Checkbox(value: false, onChanged: (value) {}, checkColor: Colors.white,),
@@ -119,10 +57,10 @@ class SignInForm extends StatelessWidget {
           ],
         ), 
         const SizedBox(height: 30),
-        SignInBtn(btntitle: 'Sign In',), 
+        Center(child: SignInBtn(btntitle: 'Sign In',)), 
         const SizedBox(height: 20), 
-        const Text('- OR - ', style: TextStyle(color: Colors.white, fontSize: 16),), 
-        const SizedBox(height: 10), 
+        const Center(child: Text('- OR - ', style: TextStyle(color: Colors.white, fontSize: 16),)), 
+         const SizedBox(height: 10), 
          Padding(
            padding: const EdgeInsets.symmetric(horizontal: 100.0),
            child: Row(
@@ -133,12 +71,81 @@ class SignInForm extends StatelessWidget {
             ],
             ),
          ),
-         SizedBox(height: 20), 
+         const SizedBox(height: 30), 
       
-        const Center(child: Text('Create Account', style: TextStyle(color: Colors.white, fontSize: 18),),)
+        const Center(child: Text('Create Account', style: TextStyle(color: Colors.white, fontSize: 18),),), 
+SizedBox(height: 80), 
+            ],
+          ),
+        ),
+        ),
+      )
+    );
+  }
+}
 
+class SignInForm extends StatelessWidget {
+  const SignInForm({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(child: Column(
+      children: [
+        signInEmail(), 
+        const SizedBox(height: 20), 
+        signInPwd(), 
+        
       ],
     ));
+  }
+
+  Container signInPwd() {
+    return Container(
+         height: 55,
+        decoration:  BoxDecoration(
+           color: Colors.grey.shade200,
+           borderRadius: BorderRadius.circular(10), 
+        ),
+       
+       
+        child: const Align(
+          child: TextField(
+            obscureText: true,
+            textInputAction: TextInputAction.done,
+            decoration: InputDecoration(
+              floatingLabelBehavior: FloatingLabelBehavior.always,
+              border: InputBorder.none,
+              
+              hintText: 'Enter Password', hintStyle: TextStyle(color: Colors.black),
+              prefixIcon: Icon(Icons.key,)
+            )
+          ),
+        ),
+      );
+  }
+
+  Container signInEmail() {
+    return Container(
+         height: 55,
+        decoration:  BoxDecoration(
+           color: Colors.grey.shade200,
+           borderRadius: BorderRadius.circular(10), 
+        ),
+       
+       
+        child: const Align(
+          child: TextField(
+            textInputAction: TextInputAction.next,
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              hintText: 'Enter Email', hintStyle: TextStyle(color: Colors.black),
+              prefixIcon: Icon(Icons.email_outlined,)
+            )
+          ),
+        ),
+      );
   }
 }
 
