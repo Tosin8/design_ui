78c26684666a255c0f05730a54c9730b683ca3a3
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-01-12 17:07:36 +0100

    Update design_form.dart

--- lib/screens/form/designed_form/design_form.dart
+++ lib/screens/form/designed_form/design_form.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 
-class DesignForm extends StatelessWidget {
+class DesignForm extends StatefulWidget {
   const DesignForm({super.key});
 
   @override
+  State<DesignForm> createState() => _DesignFormState();
+}
+
+final bkg = AssetImage('assets/form/1.jpg');
+class _DesignFormState extends State<DesignForm> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold( 
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(image: bkg)
+        ),
+      )
+    );
   }
 }
