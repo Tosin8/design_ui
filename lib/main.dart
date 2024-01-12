b1f20729c9dcedea59be817d50b6cbf50c7ccd4c
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-01-12 13:53:42 +0100

    Creating animated form

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ui_design/screens/animated_form/home_form.dart';
 import 'package:ui_design/screens/furniture_gridview/gridview.dart';
 import 'package:ui_design/screens/furniture_ui/onboarding.dart';
 
@@ -22,7 +23,7 @@ class MyApp extends StatelessWidget {
     theme: ThemeData(
       primarySwatch: Colors.blue,
     ),
-    home: const Discover(), 
+    home: const Home_Form(), 
 
     ); 
   }
---
+++ lib/screens/animated_form/home_form.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Home_Form extends StatefulWidget {
+  const Home_Form({super.key});
+
+  @override
+  State<Home_Form> createState() => _Home_FormState();
+}
+
+class _Home_FormState extends State<Home_Form> {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of
+        (context).size;
+    return Scaffold(
+      backgroundColor: Color(0xff192028), 
+      body: ScrollConfiguration(child: SizedBox(height: size.height,)),
+    );
+  }
+}
