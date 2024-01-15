bbbda660d77b347d4a83578f9ef2b8677e798e73
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-01-15 16:22:49 +0100

    adding vertical card pager

--- lib/screens/smatch_watch/smart.dart
+++ lib/screens/smatch_watch/smart.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:vertical_card_pager/vertical_card_pager.dart';
 
 class Smart extends StatefulWidget {
   const Smart({super.key});
@@ -10,6 +11,16 @@ class Smart extends StatefulWidget {
 class _SmartState extends State<Smart> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Padding(
+        padding: EdgeInsets.all(30), 
+        child: Column(
+          children: [
+            Expanded(child: VerticalCardPager(titles: titles, images: images))
+          ],
+        ),
+        ),
+    );
   }
 }
