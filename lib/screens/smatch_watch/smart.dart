3a54fdfad7a72b44ffb0fffcacdaa7a16f48f372
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-01-15 16:45:41 +0100

    Update smart.dart

--- lib/screens/smatch_watch/smart.dart
+++ lib/screens/smatch_watch/smart.dart
@@ -9,15 +9,42 @@ class Smart extends StatefulWidget {
 }
 
 class _SmartState extends State<Smart> {
+
+// final images = [
+//   'assets/smart/music.jpg',
+//   'assets/smart/alarm.jpg',
+//   'assets/smart/calendar.jpg', 
+//   'assets/smart/settings.jpg'
+// ]; 
+
+final images = [
+  Container(color: Colors.red,), 
+  Container(color: Colors.yellow,), 
+  Container(color: Colors.amber,), 
+  Container(color: Colors.green,)
+];
+final titles = [
+  'Music', 'Alarm', 'Calendar', 'Settings'
+]; 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: const Padding(
+      body:Padding(
         padding: EdgeInsets.all(30), 
         child: Column(
           children: [
-            Expanded(child: VerticalCardPager(titles: titles, images: images))
+            Expanded(
+              child: VerticalCardPager(
+                titles: titles, 
+                images: images, 
+              // images: images.map((image) => Image.asset(images as String)).toList(),
+
+
+              onPageChanged: (page){print(page);
+               },
+              ), 
+              )
           ],
         ),
         ),
