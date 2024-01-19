7a34b414b9545f08d70fa1d9781ef77521c6ce3e
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-01-19 13:08:47 +0100

    Creating for container transform

---
+++ lib/screens/materia_io/container_transform/container_transform.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class OpenContainerTransform extends StatelessWidget {
+  const OpenContainerTransform({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
