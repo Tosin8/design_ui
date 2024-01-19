902c5a2898e9fca6eeaf852f49ba0d10ce8f4079
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-01-19 13:09:58 +0100

    Update container_transform.dart

--- lib/screens/materia_io/container_transform/container_transform.dart
+++ lib/screens/materia_io/container_transform/container_transform.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 
+const double _fabDimension = 56.0; 
+
 class OpenContainerTransform extends StatelessWidget {
   const OpenContainerTransform({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Primary Screen')
+      ),
+    );
   }
 }
