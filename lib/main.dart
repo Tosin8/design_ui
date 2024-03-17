e8a513c94e642b3a9f9194359efc54ff06b932b3
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-17 15:08:08 +0100

    adding pub animations

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:ui_design/screens/form/designed_form/design_form.dart';
 import 'package:ui_design/screens/materia_io/container_transform/container_transform.dart';
-import 'package:ui_design/screens/smatch_watch/smart.dart';
 
 
 
