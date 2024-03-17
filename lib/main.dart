e3f8f1b4893a7c6829aa06511c8979b5cb6ac4fa
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-17 15:40:23 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:ui_design/screens/travel_onboard/swipe_home.dart';
+import 'package:ui_design/screens/smatch_watch/smart.dart';
 
 
 
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
     theme: ThemeData(
       primarySwatch: Colors.blue,
     ),
-    home: const HomeSwipe(), 
+    home: const Smart(), 
 
     ); 
   }
