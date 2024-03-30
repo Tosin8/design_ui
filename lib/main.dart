0dd8f550a637ba272b38d7348f0c84761b159c63
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-03-30 02:44:20 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 
 import 'screens/discover_ui/discover.dart';
+import 'screens/furniture_app/home/home.dart';
 import 'screens/furniture_gridview/gridview.dart';
 import 'screens/furniture_ui/onboarding.dart';
 import 'screens/shoe_app/home.dart';
+import 'screens/shoe_app/shoe_ui.dart';
 
 
 
@@ -23,7 +25,7 @@ class MyApp extends StatelessWidget {
     theme: ThemeData(
       primarySwatch: Colors.blue,
     ),
-    home: const ShoeHome (), 
+    home: const FurnitureHome (), 
 
     ); 
   }
