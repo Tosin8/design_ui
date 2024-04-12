5d08aa6cca12b8e5465149426214a5a1efd19223
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-04-12 13:06:11 +0100

    adding readmore

--- lib/screens/furniture_gridview/detail_screen/product_detail.dart
+++ lib/screens/furniture_gridview/detail_screen/product_detail.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:readmore/readmore.dart';
 import 'package:ui_design/screens/furniture_gridview/model/product.dart';
 
 class ProductDetails extends StatelessWidget {
@@ -30,23 +32,49 @@ class ProductDetails extends StatelessWidget {
             ],
                    ),
                   
-                 
-    
-    
     body: Stack(
       children: [
          Container(
               child: 
               Column(
                 children: [
-                  Expanded(
-                    child: Hero(
+                  Hero(
                       tag: 'products[0].image',
                       child: Container(
+                        height: 400,
                         child: Image.asset(products[0].image,  fit: BoxFit.cover,),
                       ),
-                    )
-                  )
+                    
+                  ), 
+                  const SizedBox(height: 20),
+                  Padding( 
+                    padding: const EdgeInsets.all(5),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(products[0].title,
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                         style: const TextStyle(fontWeight: FontWeight.w700, fontSize: 18),
+                        ), 
+                        Text('\$${products[0].price.toString()}', style: const TextStyle(fontWeight: FontWeight.w700, fontSize: 18),)
+                      ],
+                    ),
+                  ), 
+                  const SizedBox(height: 10,), 
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ReadMoreText(
+                      products[0].description, 
+                      trimLength:3, 
+                      trimMode: TrimMode.Line,
+                      colorClickableText: Colors.blue,
+  trimCollapsedText: 'Show more',
+  trimExpandedText: 'Show less',
+  moreStyle: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold, color: Colors.blue),
+  lessStyle: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold, color: Colors.blue),
+                      style: TextStyle(color: Colors.grey.shade500),),
+                  ),
                 ],
               ),
             ),
--- lib/screens/furniture_gridview/model/product.dart
+++ lib/screens/furniture_gridview/model/product.dart
@@ -3,52 +3,54 @@ class Product {
   final String title; 
   final String image; 
   final int price;
-  Product({
-    required this.title,
-    required this.image,
-    required this.price,
-  });
+  final String description;
+
+  Product({required this.title, required this.image, required this.price, required this.description}); 
+ 
  
 }
 
 List<Product> products = [
 Product(
+  
   price: 1200,
-  title: 'Product 1',
- image: 'assets/furniture_grid/1.jpg'
+  title: 'Nova Nordic Dining Chair',
+ image: 'assets/furniture_grid/1.jpg', 
+ description: 'This chair is an elegant and functional piece of furniture. It is suitable for family visits and parties with friends and perfect for relaxing in front of the TV after hard work.',
  ), 
  Product(
   title: 'Product 2',
  image: 'assets/furniture_grid/2.jpg', 
- price: 800
+ price: 800, 
+ description: ''
  ), 
  Product(
   title: 'Product 3',
  image: 'assets/furniture_grid/3.jpg', 
- price: 680
+ price: 680, description: ''
  ), 
  Product(
   title: 'Product 1',
  image: 'assets/furniture_grid/4.jpg', 
- price: 400
+ price: 400, description: ''
  ), 
  Product(
   title: 'Product 1',
  image: 'assets/furniture_grid/5.jpg', 
- price: 450
+ price: 450, description: ''
  ), 
  Product(
   title: 'Product 1',
  image: 'assets/furniture_grid/6.jpg', 
- price: 820
+ price: 820, description: ''
  ), 
  Product(
   title: 'Product 1',
  image: 'assets/furniture_grid/7.jpg', 
- price: 460
+ price: 460, description: ''
  ), Product(
   title: 'Product 1',
  image: 'assets/furniture_grid/8.jpg', 
- price: 800
+ price: 800, description: ''
  ), 
 ]; 
