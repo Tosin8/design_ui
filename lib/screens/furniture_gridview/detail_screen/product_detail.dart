dd67c339b789812dbf3d5707dbb03292a668c1e7
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-04-12 11:20:09 +0100

    Update product_detail.dart

--- lib/screens/furniture_gridview/detail_screen/product_detail.dart
+++ lib/screens/furniture_gridview/detail_screen/product_detail.dart
@@ -1,36 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:ui_design/screens/furniture_gridview/model/product.dart';
 
 class ProductDetails extends StatelessWidget {
   const ProductDetails({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        leading: IconButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          icon: const Icon(
-            Icons.arrow_back_ios,
-            color: Colors.black,
-          ),
-          
-        ), 
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.favorite_outline, color: Colors.black),
-          )
-        ],
-      ),
-      body: Stack(
-        children: [
-          
-        ],
-      ),
-    );
+    return  Scaffold(
+            extendBodyBehindAppBar: true,
+                   appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            leading: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: const Icon(
+                Icons.arrow_back_ios,
+                color: Colors.black,
+              ),
+              
+            ), 
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.favorite_outline, color: Colors.black),
+              )
+            ],
+                   ),
+                  
+                 
+    
+    
+    body: Stack(
+      children: [
+         Container(
+              child: 
+              Column(
+                children: [
+                  Expanded(
+                    child: Hero(
+                      tag: 'products[0].image',
+                      child: Container(
+                        child: Image.asset(products[0].image,  fit: BoxFit.cover,),
+                      ),
+                    )
+                  )
+                ],
+              ),
+            ),
+         
+         
+    ]));
   }
 }
