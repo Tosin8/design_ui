7622b6b48cb321ddfdc2e7a5caa46ff8fe35e9d2
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-04-28 18:48:15 +0100

    Update home.dart

--- lib/screens/sneek/shop/home.dart
+++ lib/screens/sneek/shop/home.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class SneekHome extends StatelessWidget {
   const SneekHome({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sneek'), 
+        centerTitle: true,
+      )
+    );
   }
 }
