6f7c12c594cfce210363d74effbb4d624d7aa9ff
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-05-05 17:41:18 +0100

    Refactoring folders

--- lib/screens/coffee_shop/screen/shop.dart
+++ lib/screens/coffee_shop/screen/shop.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
 class CoffeeShop extends StatelessWidget {
   const CoffeeShop({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        leading: GestureDetector(
+          onTap: () {
+            
+          },
+          child: const Icon(Iconsax.message)),
+          actions: const [
+             CircleAvatar(
+              radius: 10,
+              backgroundImage: AssetImage('assets/coffee_shop/2.jpg'),
+            )
+          ],
+      ),
+    );
   }
 }
