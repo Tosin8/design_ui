d30d81914d313e6b0db1c9fac6cd932270ff8868
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-05-05 19:35:34 +0100

    Update shop.dart

--- lib/screens/coffee_shop/screen/shop.dart
+++ lib/screens/coffee_shop/screen/shop.dart
@@ -21,6 +21,43 @@ class CoffeeShop extends StatelessWidget {
             SizedBox(width: 10,), 
           ],
       ),
+      body:  Column(
+        mainAxisAlignment: MainAxisAlignment.start, 
+        crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Text('Find the best \ncoffee for you', 
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),),
+            ),
+            
+            const SizedBox(height: 8,), 
+            Padding(
+              padding: const EdgeInsets.only(left: 12.0, right: 12.0),
+              child: TextFormField(
+                  decoration: InputDecoration(
+                    
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10), 
+                    ),
+                    focusColor: Colors.grey[400],
+                    focusedBorder: OutlineInputBorder(
+                       borderRadius: BorderRadius.circular(10), 
+                    ),
+                    prefixIcon: const Icon(Icons.search), 
+                    hintText: 'Search your coffee',
+                  ),
+                ),
+            ), 
+            SizedBox(height: 10,), 
+            Text('Categories', style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),),
+                  
+                
+          ] 
+          
+          
+        ),
+      
     );
   }
 }
