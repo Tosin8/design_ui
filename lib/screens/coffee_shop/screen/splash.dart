8c642cb299882e8b42ad1d172cf6f8616eb251f5
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-05-05 02:02:14 +0100

    Update splash.dart

--- lib/screens/coffee_shop/screen/splash.dart
+++ lib/screens/coffee_shop/screen/splash.dart
@@ -5,6 +5,16 @@ class CoffeeSplash extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image:
+          AssetImage(
+            'assets/coffee_shop/1.jpg',
+          ), fit: BoxFit.cover))
+        ),
+      
+    );
   }
 }
