a83db93823067800f3607cf3fadcdb5375963466
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-05-05 19:08:37 +0100

    Update coffee_shop.dart

--- lib/screens/coffee_shop/model/coffee_shop.dart
+++ lib/screens/coffee_shop/model/coffee_shop.dart
@@ -47,15 +47,23 @@ final List<Coffee> _shop = [
 ]; 
 
 // user cart
-
+List<Coffee> _userCart =  []; 
 
 // get coffee list
+List<Coffee> get coffeeShop => _shop; 
 
 // get user cart
+List<Coffee> get userCart => _userCart; 
+
 
 // add item to cart
-
+void addItemToCart(Coffee coffee){
+  _userCart.add(coffee);
+  }
 
 // remove item from cart. 
-
+void removeItemFromCart(Coffee coffee){
+  _userCart.remove(coffee);
+  }
 }
+
