b4215ca4972522aa71aef12e52a18e85b4f521c7
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-05-11 23:53:20 +0100

    Navigating Routeing

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:ui_design/screens/minimal_shop/screens/shop.dart';
+import 'package:ui_design/screens/minimal_shop/screens/splash.dart';
 
 
 
@@ -21,7 +22,7 @@ class MyApp extends StatelessWidget {
     theme: ThemeData(
       primarySwatch: Colors.blue,
     ),
-    home: const MinimalShop (), 
+    home: const MinimalSplash (), 
 
     ); 
   }
--- lib/screens/minimal_shop/screens/splash.dart
+++ lib/screens/minimal_shop/screens/splash.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:ui_design/screens/minimal_shop/screens/shop.dart';
 
 class MinimalSplash extends StatelessWidget {
   const MinimalSplash({super.key});
@@ -10,7 +12,37 @@ class MinimalSplash extends StatelessWidget {
       backgroundColor: Theme.of(context).colorScheme.background,
       body: Center(
         child: Column(
-          
+
+          mainAxisAlignment: MainAxisAlignment.center, 
+          children: [
+
+            // logo
+            Icon(Icons.shopping_bag, size: 80, color: Theme.of(context).colorScheme.inversePrimary,) , 
+
+            const SizedBox(height: 25,), 
+            const Text('Minimal Shop', style: TextStyle(color: Colors.black, fontSize: 16, fontWeight: FontWeight.bold),), 
+
+             const SizedBox(height: 5,), 
+            const Text('Premium Quality Products', style: TextStyle(color: Colors.black, fontSize: 16, fontWeight: FontWeight.w300),), 
+
+            const SizedBox(height: 200,), 
+            GestureDetector( 
+              onTap: () {
+                Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const MinimalShop()),
+            );
+              },
+              child: Container(
+                height: 50, width: 200,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+                child: Align(child: Text('Enter Shop', style: TextStyle(color: Theme.of(context).colorScheme.onPrimary),)),
+              ),
+            )
+          ],
         ),
       )
     );
