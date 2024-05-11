a1ecf041647ad1bdbdd518cb313c8e03c39e72cf
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-05-11 23:27:11 +0100

    Update splash.dart

---
+++ lib/screens/minimal_shop/screens/splash.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MinimalSplash extends StatelessWidget {
+  const MinimalSplash({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(
+        child: Column(
+          
+        ),
+      )
+    );
+  }
+}
