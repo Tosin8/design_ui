d1865f1b82555846fbd541b7e710bc7dfb66d094
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-05-11 23:06:03 +0100

    Create menu_drawer.dart

---
+++ lib/screens/minimal_shop/components/menu_drawer.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class MenuDrawer extends StatelessWidget {
+  const MenuDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
