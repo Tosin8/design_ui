4466d5b1e656cd3ea7c9041148bf393a934a795d
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-05-11 22:55:42 +0100

    Creating UI for minimal shop

---
+++ lib/screens/minimal_shop/screens/shop.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MinimalShop extends StatelessWidget {
+  const MinimalShop({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold( 
+      
+    );
+  }
+}
