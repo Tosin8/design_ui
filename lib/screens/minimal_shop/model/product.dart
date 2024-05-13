53aac8994b9d8a31e16fc3eb9a4837de790d92b1
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-05-13 20:04:51 +0100

    Create shop products.

--- lib/screens/minimal_shop/model/product.dart
+++ lib/screens/minimal_shop/model/product.dart
@@ -1,10 +1,10 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
-class Product {
+class MinimalProduct {
   String name; 
   String description; 
   String price; 
   String image; 
-  Product({
+  MinimalProduct({
     required this.name,
     required this.description,
     required this.price,
---
+++ lib/screens/minimal_shop/model/shop.dart
@@ -0,0 +1,8 @@
+import 'package:ui_design/screens/minimal_shop/model/product.dart';
+
+class Shop{
+  // products for sale
+
+
+final List<MinimalProduct> _shop = []; 
+}
