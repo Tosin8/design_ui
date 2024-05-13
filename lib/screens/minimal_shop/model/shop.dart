8cc2d4b1e7c438989dd77f3dc4178d6b482fb558
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-05-13 20:22:51 +0100

    Create product tile

--- lib/screens/minimal_shop/model/shop.dart
+++ lib/screens/minimal_shop/model/shop.dart
@@ -7,9 +7,59 @@ class Shop{
 final List<MinimalProduct> _shop = [
 
   MinimalProduct(
-    name: name,
-     description: description, 
-     price: price, 
-     image: image);
+    name: 'Shoe 1',
+     description: '', 
+     price: '14.4', 
+     image: 'assets/minimal_shop/products/1.png'
+     ), 
+
+       MinimalProduct(
+    name: 'Shoe 2',
+     description: '', 
+     price: '34.5', 
+     image: 'assets/minimal_shop/products/2.png'
+     ), 
+
+       MinimalProduct(
+    name: 'Shoe 3',
+     description: '', 
+     price: '34.2', 
+     image: 'assets/minimal_shop/products/3.png'
+     ), 
+
+       MinimalProduct(
+    name: 'Shoe 4',
+     description: '', 
+     price: '12.0', 
+     image: 'assets/minimal_shop/products/4.png'
+     ), 
+
+       MinimalProduct(
+    name: 'Shoe 5',
+     description: '', 
+     price: '51.0', 
+     image: 'assets/minimal_shop/products/5.png'
+     ), 
 ]; 
+
+// User Cart 
+List<MinimalProduct> _cart = []; 
+
+// Get Product List 
+List<MinimalProduct> get shop => _shop; 
+
+// Get User Cart
+List<MinimalProduct> get cart => _cart; 
+
+// Add item to cart
+void addToCart(MinimalProduct item) {
+  _cart.add(item);
+}
+
+// remove item from cart 
+void removeFromCart(MinimalProduct item){
+  _cart.remove(item); 
+}
+
+
 }
