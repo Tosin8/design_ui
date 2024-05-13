681ce1940ab0d8facf02ce2b0dd2c0a1cc590d9a
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-05-13 20:23:51 +0100

    Update product_tile.dart

--- lib/screens/minimal_shop/components/product_tile.dart
+++ lib/screens/minimal_shop/components/product_tile.dart
@@ -1,8 +1,15 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 
-class ProductTile extends StatelessWidget {
-  const ProductTile({super.key});
+import 'package:ui_design/screens/minimal_shop/model/product.dart';
 
+class ProductTile extends StatelessWidget {
+  const ProductTile({
+    Key? key,
+    required this.minimalProduct,
+  }) : super(key: key);
+
+final MinimalProduct minimalProduct; 
   @override
   Widget build(BuildContext context) {
     return Container();
