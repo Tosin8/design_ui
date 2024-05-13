911bf774c03eb59a082bbb458bc53d92e24e7465
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-05-13 23:52:55 +0100

    Update cart.dart

--- lib/screens/minimal_shop/screens/cart.dart
+++ lib/screens/minimal_shop/screens/cart.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../components/app_drawer.dart';
+import '../model/shop.dart';
 
 class MinimalCartScreen extends StatelessWidget {
   const MinimalCartScreen({super.key});
 
+
+// remove item from cart method 
+void removeItemFromCart(BuildContext context) {}
   @override
   Widget build(BuildContext context) {
+
+    // get access to the cart. 
+
+    final cart = context.watch<Shop>().cart;
     return Scaffold(
       backgroundColor: Colors.grey[300],
       drawer: const MinimalDrawer(),
@@ -15,6 +24,27 @@ class MinimalCartScreen extends StatelessWidget {
        title: const Text('Minimal Cart', style: TextStyle(color: Colors.black, fontSize: 16),), 
       centerTitle: true,
       ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemBuilder: (context, index) {
+
+                // get individual item to cart. 
+                final item = cart[index];
+
+                // return as a cart title UI. 
+                return ListTile(
+                  title: Text(item.name), 
+                  subtitle: Text(item.price.toString(), 
+                  ),
+                  trailing: IconButton(
+                    onPressed: () => removeItemFromCart(context), icon: Icon(Icons.remove)),
+                ); 
+              }, 
+              itemCount: cart.length)),
+        ],
+      )
     );
   }
 }
