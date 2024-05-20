dc4d60203806111d320a0e3be56ddf2e8fa96d6b
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-05-20 19:26:37 +0100

    Create size_config.dart

---
+++ lib/screens/hommy/widgets/size_config.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class SizeConfig{
+  static MediaQueryData _mediaQueryData; 
+  static double screenWidth;
+  static double screenHeight;
+  static double defaultSize;
+  static Orientation orientation;
+}
