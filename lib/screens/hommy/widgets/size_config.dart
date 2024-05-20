0410170acd0d05b55edee66214f3bc881ba103e2
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-05-20 19:28:32 +0100

    Update size_config.dart

--- lib/screens/hommy/widgets/size_config.dart
+++ lib/screens/hommy/widgets/size_config.dart
@@ -6,4 +6,15 @@ class SizeConfig{
   static double screenHeight;
   static double defaultSize;
   static Orientation orientation;
+
+  void init(BuildContext context){
+    _mediaQueryData = MediaQuery.of(context);
+    screenWidth = _mediaQueryData.size.width;
+    screenHeight = _mediaQueryData.size.height;
+    orientation = _mediaQueryData.orientation;
+
+    defaultSize = orientation == Orientation.landscape ? 
+    screenHeight * 0.024
+     : screenWidth * 0.024;
+  }
 }
