99dab476b49a86ced1f3ffe97941fd6d60929715
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-05-20 19:54:12 +0100

    Update categories.dart

--- lib/screens/hommy/models/categories.dart
+++ lib/screens/hommy/models/categories.dart
@@ -1,5 +1,45 @@
 class Category {
   final String id, title, image; 
   final int numOfProducts; 
+
+
   Category({required this.id, required this.title, required this.image, required this.numOfProducts});
+
+  // Creating category from JSON. 
+  factory Category.fromJson(Map<String, dynamic> json) {
+    return Category(
+      id: json['id'],
+      title: json['title'],
+      image: json['image'],
+      numOfProducts: json['numOfProducts'],
+    );
+  }
 }
+
+// List of categories
+// List<Category> categories = [
+//   Category(
+//     id: "1",
+//     title: "Chair",
+//     image: "https://i.imgur.com/sI4GvE6.png",
+//     numOfProducts: 100,
+//   ),
+//   Category(
+//     id: "2",
+//     title: "Sofa",
+//     image: "https://i.imgur.com/sI4GvE6.png",
+//     numOfProducts: 100,
+//   ),
+//   Category(
+//     id: "3",
+//     title: "Table",
+//     image: "https://i.imgur.com/sI4GvE6.png",
+//     numOfProducts: 100,
+//   ),
+
+// category
+Category category = Category(id: '1',
+ title: 'Arm Chair', 
+ image: '',
+  numOfProducts: 100
+  ); 
