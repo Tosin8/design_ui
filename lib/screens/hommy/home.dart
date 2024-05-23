023e72f33c8cd2962d628bcbbbf43c074b9ee4e8
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-05-23 17:30:28 +0100

    Update home.dart

--- lib/screens/hommy/home.dart
+++ lib/screens/hommy/home.dart
@@ -63,23 +63,26 @@ class Hommy1 extends StatelessWidget {
                   title: 'Recommends for you',
                 ),
                 const SizedBox(height: 5,),
-                Container(
-                  height: 300, width: 180,
-                 
-                  child:  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        height: 200, width: 180,
-                        decoration: BoxDecoration(image: const DecorationImage(image: AssetImage('assets/furniture_grid/1.jpg',), fit: BoxFit.cover), borderRadius: BorderRadius.circular(15)),
-                      ), 
-                      const SizedBox(height: 5,), 
-                      const Text('White Sofa', style: TextStyle(fontWeight: FontWeight.w500, fontSize: 18),), 
-                      Text('\$700', 
-                      style: TextStyle(fontWeight: FontWeight.w500, fontSize: 16, 
-                      color: Colors.black.withOpacity(0.5)),
-                      ), 
-                    ], 
+                GestureDetector( 
+                  onTap: () {},
+                  child: Container(
+                    height: 300, width: 180,
+                   
+                    child:  Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          height: 200, width: 180,
+                          decoration: BoxDecoration(image: const DecorationImage(image: AssetImage('assets/furniture_grid/1.jpg',), fit: BoxFit.cover), borderRadius: BorderRadius.circular(15)),
+                        ), 
+                        const SizedBox(height: 5,), 
+                        const Text('White Sofa', style: TextStyle(fontWeight: FontWeight.w500, fontSize: 18),), 
+                        Text('\$700', 
+                        style: TextStyle(fontWeight: FontWeight.w500, fontSize: 16, 
+                        color: Colors.black.withOpacity(0.5)),
+                        ), 
+                      ], 
+                    ),
                   ),
                 )
 
