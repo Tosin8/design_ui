a0e3c3460664e13b5c5034bd2a9cdd36dbc8de14
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-05-25 22:36:45 +0100

    Update fancy_refresh_home.dart

--- lib/screens/fancy_refresh/fancy_refresh_home.dart
+++ lib/screens/fancy_refresh/fancy_refresh_home.dart
@@ -4,14 +4,21 @@ import 'package:liquid_pull_to_refresh/liquid_pull_to_refresh.dart';
 class FancyRefreshPage extends StatelessWidget {
   const FancyRefreshPage({super.key});
 
+  Future<void> _handleRefresh() async {
+    await Future.delayed(const Duration(seconds: 3));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.deepPurple[200],
       body: LiquidPullToRefresh( 
-        onRefresh: () {
-          
-        },
+        onRefresh: _handleRefresh, 
+        color: Colors.deepPurple, 
+        backgroundColor: Colors.deepPurple[200],
+        showChildOpacityTransition: false,
+        animSpeedFactor: 3,
+        height: 300,
         child: ListView(
           children: [
             Padding(padding: 
