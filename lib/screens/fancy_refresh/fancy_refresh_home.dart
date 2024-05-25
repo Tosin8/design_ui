f1f359b58421098e06097cb5782eb88f09bdfe7f
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-05-25 22:33:43 +0100

    Update fancy_refresh_home.dart

--- lib/screens/fancy_refresh/fancy_refresh_home.dart
+++ lib/screens/fancy_refresh/fancy_refresh_home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:liquid_pull_to_refresh/liquid_pull_to_refresh.dart';
 
 class FancyRefreshPage extends StatelessWidget {
   const FancyRefreshPage({super.key});
@@ -7,33 +8,38 @@ class FancyRefreshPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.deepPurple[200],
-      body: ListView(
-        children: [
-          Padding(padding: 
-            const EdgeInsets.all(20), 
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(20), 
-              child: Container( 
-                height: 300, color: Colors.deepPurple,
-              ),
-            ),), 
-             Padding(padding: 
-            const EdgeInsets.all(20), 
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(20), 
-              child: Container( 
-                height: 300, color: Colors.deepPurple,
-              ),
-            ),), 
-             Padding(padding: 
-            const EdgeInsets.all(20), 
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(20), 
-              child: Container( 
-                height: 300, color: Colors.deepPurple,
-              ),
-            ),)
-        ],
+      body: LiquidPullToRefresh( 
+        onRefresh: () {
+          
+        },
+        child: ListView(
+          children: [
+            Padding(padding: 
+              const EdgeInsets.all(20), 
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20), 
+                child: Container( 
+                  height: 300, color: Colors.deepPurple,
+                ),
+              ),), 
+               Padding(padding: 
+              const EdgeInsets.all(20), 
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20), 
+                child: Container( 
+                  height: 300, color: Colors.deepPurple,
+                ),
+              ),), 
+               Padding(padding: 
+              const EdgeInsets.all(20), 
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20), 
+                child: Container( 
+                  height: 300, color: Colors.deepPurple,
+                ),
+              ),)
+          ],
+        ),
       )
     );
   }
