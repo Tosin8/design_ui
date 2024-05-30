9f9ab44b9a39fb772f214c5ee6478342f5378e9a
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-05-30 20:41:49 +0100

    Update dashboard_home.dart
    adding orientaiton builder

--- lib/screens/dashboard/dashboard_home.dart
+++ lib/screens/dashboard/dashboard_home.dart
@@ -12,45 +12,9 @@ class _DashboardHomeState extends State<DashboardHome> {
   Widget build(BuildContext context) {
 
 //BREAKPOINT:
-      bool isDesktop(BuildContext context) => MediaQuery.of(context).size.width >= 600;
-       bool isMobile(BuildContext context) => MediaQuery.of(context).size.width < 600;
-    double screenWidth = MediaQuery.of(context).size.width;
-    double screenHeight = MediaQuery.of(context).size.height;
-    return  Scaffold( 
+     return OrientationBuilder(builder: (context, orientation) {
 
-      appBar: AppBar(
-        backgroundColor: Colors.yellow,
-        title: isDesktop(context) ? const Text('Desktop') :
-        const Text('Mobile'),
-         centerTitle: true,
-      ),
-
-      // KNOWING THE NUMBER SIZE: 
-      // body: Center(
-      //   child: Text('$screenHeight, $screenWidth'), 
-      // ), 
-
-      body: Row(
-        children: [
-          if(isDesktop(context))
-          Container(
-            width: 200 , 
-            height: screenHeight,
-            color: Colors.blue,
-            child: const Center(child: Text('SIDEBAR')),
-          ),
-          Expanded(
-          
-            child: Container(
-              width: screenWidth , 
-              height: screenHeight,
-              color: Colors.red,
-              child: const Center(child: Text('CONTENT'),),
-            ),
-          ),
-        ],
-      )
-
-    );
+     }); 
+    
   }
 }
