655bd6c477e695710485dbdbf76e8067105f4f56
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-05-30 18:55:24 +0100

    Creating UI Dashboard

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:provider/provider.dart';
 import 'package:ui_design/screens/dashboard/dashboard_home.dart';
-import 'package:ui_design/screens/fancy_refresh/fancy_refresh_home.dart';
-import 'package:ui_design/screens/hommy/splash_screen.dart';
 import 'package:ui_design/screens/minimal_shop/model/shop.dart';
 
-import 'package:ui_design/screens/minimal_shop/screens/splash.dart';
 
 
 
--- lib/screens/dashboard/dashboard_home.dart
+++ lib/screens/dashboard/dashboard_home.dart
@@ -10,6 +10,35 @@ class DashboardHome extends StatefulWidget {
 class _DashboardHomeState extends State<DashboardHome> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+    return  Scaffold( 
+
+      // KNOWING THE NUMBER SIZE: 
+      // body: Center(
+      //   child: Text('$screenHeight, $screenWidth'), 
+      // ), 
+
+      body: Row(
+        children: [
+          Container(
+            width: 200 , 
+            height: screenHeight,
+            color: Colors.blue,
+            child: const Center(child: Text('SIDEBAR')),
+          ),
+          Expanded(
+            child: Container(
+              width: screenWidth , 
+              height: screenHeight,
+              color: Colors.red,
+              child: const Center(child: Text('CONTENT'),),
+            ),
+          ),
+        ],
+      )
+
+    );
   }
 }
