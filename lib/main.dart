df1cbc7201425b017ad4736904b9fb3b07ae2396
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-05-30 18:13:19 +0100

    Create UI for Dashboard

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:provider/provider.dart';
+import 'package:ui_design/screens/dashboard/dashboard_home.dart';
 import 'package:ui_design/screens/fancy_refresh/fancy_refresh_home.dart';
 import 'package:ui_design/screens/hommy/splash_screen.dart';
 import 'package:ui_design/screens/minimal_shop/model/shop.dart';
@@ -52,7 +53,7 @@ class MyApp extends StatelessWidget {
     theme: ThemeData(
       primarySwatch: Colors.blue,
     ),
-    home: const FancyRefreshPage(), 
+    home: const DashboardHome(), 
 
     ); 
   }
---
+++ lib/screens/dashboard/dashboard_home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DashboardHome extends StatefulWidget {
+  const DashboardHome({super.key});
+
+  @override
+  State<DashboardHome> createState() => _DashboardHomeState();
+}
+
+class _DashboardHomeState extends State<DashboardHome> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
