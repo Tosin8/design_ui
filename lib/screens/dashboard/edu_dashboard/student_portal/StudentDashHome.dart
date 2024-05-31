5e6bddd1524f16d83f6c4b603d083c85efe3a472
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-05-31 22:11:50 +0100

    Create StudentDashHome.dart

---
+++ lib/screens/dashboard/edu_dashboard/student_portal/StudentDashHome.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class StudentDashHome extends StatelessWidget {
+  const StudentDashHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
