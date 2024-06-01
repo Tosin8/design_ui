bcca27db5247d5c001c43c35c86a77e209d3cf90
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-06-01 03:14:43 +0100

    Update middlescreen.dart

---
+++ lib/screens/dashboard/edu_dashboard/student_portal/boards/middlescreen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MiddleScreen extends StatelessWidget {
+  const MiddleScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
