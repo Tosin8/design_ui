d6166e0af19e8679b194300e0072887cf6178d10
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-06-01 02:57:42 +0100

    updating with image

--- lib/screens/dashboard/edu_dashboard/student_portal/boards/sideMenu.dart
+++ lib/screens/dashboard/edu_dashboard/student_portal/boards/sideMenu.dart
@@ -1,5 +1,9 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:iconsax/iconsax.dart';
 
 class SideMenu extends StatelessWidget {
   const SideMenu({super.key});
@@ -13,13 +17,127 @@ class SideMenu extends StatelessWidget {
           borderRadius: BorderRadius.circular(20),
            color: const Color(0xff000000),
         ),
-        child: const Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('Samyie', style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),)
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text('Samyie', style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold, wordSpacing: 3.0),), 
+              const SizedBox(height: 30,), 
+
+              // nav. bar menu 
+              NavMenu(
+                title: 'Dashboard', 
+                onTap: () {  },
+                 iconLeading: (Iconsax.activity
+                ), 
+              
+              ), 
+                NavMenu(
+                title: 'My Courses', 
+                onTap: () {  },
+                 iconLeading: (Iconsax.book
+                ),
+              
+              )  ,NavMenu(
+                title: 'Inbox', 
+                onTap: () {  },
+                 iconLeading: (Iconsax.message
+                ),
+              
+              ) , NavMenu(
+                title: 'Notifications', 
+                onTap: () {  },
+                 iconLeading: (Iconsax.notification
+                ),
+              
+              ) , NavMenu(
+                title: 'Calendar', 
+                onTap: () {  },
+                 iconLeading: (Iconsax.calendar
+                ),
+              
+              ),   NavMenu(
+                title: 'Community', 
+                onTap: () {  },
+                 iconLeading: (Iconsax.user
+                ),
+              
+              ) ,  NavMenu(
+                title: 'Settings', 
+                onTap: () {  },
+                 iconLeading: (Iconsax.setting
+                ),
+              
+              ),
+
+              SizedBox(height: 100,),
+              Container(
+                width: 200, height: 150,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10), color: Colors.yellowAccent, 
+                ), 
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                  
+                      Text('Ask', style: TextStyle(fontSize: 18),), 
+                        Text('Samyie AI', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),), 
+                    ],
+                  ),
+                ),
+              ), 
+            ],
+          ),
         ),
         ),
+    );
+  }
+}
+
+class NavMenu extends StatelessWidget {
+  NavMenu({
+    Key? key,
+    //required this.index,
+    required this.title,
+    required this.iconLeading,
+   
+    required this.onTap, 
+  }) :  super(key: key);
+//final int index; 
+
+//int selectedIndex = 0; 
+final String title; 
+GestureTapCallback onTap; 
+final IconData iconLeading; 
+
+
+//bool  isSelected = selectedIndex == index; 
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 2.0),
+      child: InkWell(
+      hoverColor: Colors.yellow,
+      
+        onTap: onTap, 
+      
+        child: ListTile(
+          onTap: () {
+            
+          },
+          splashColor: Colors.blue,
+          hoverColor: Colors.yellow,
+          selectedColor: Colors.yellow,
+          leading: Icon(iconLeading, color: Colors.white,),
+          title: Text(title,style: const TextStyle(color: Colors.white),),
+       
+        
+        ),
+      ),
     );
   }
 }
