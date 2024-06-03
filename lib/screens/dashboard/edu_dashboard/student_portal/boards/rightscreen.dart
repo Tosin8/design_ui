9bed1ecaa74fc5aff38fbfd5e2bc8775fc517317
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-06-03 20:19:51 +0100

    Update rightscreen.dart

--- lib/screens/dashboard/edu_dashboard/student_portal/boards/rightscreen.dart
+++ lib/screens/dashboard/edu_dashboard/student_portal/boards/rightscreen.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class Rightscreen extends StatelessWidget {
   const Rightscreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container( 
-       
+    return ListView( 
+       scrollDirection: Axis.vertical, 
+       children: [
+        Container(
+          child:  Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+
+              // Premium Card
+              Container(
+                height: 300, 
+                width: 300, 
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10), 
+                ),
+              )
+            ],
+          ),
+        )
+       ],
 
     );
   }
