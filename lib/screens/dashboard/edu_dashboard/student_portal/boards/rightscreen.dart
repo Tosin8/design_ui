85fdd04e62b60e9e808f0e3ef81a11f1e809eb3b
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-06-03 21:23:08 +0100

    adding models

--- lib/screens/dashboard/edu_dashboard/student_portal/boards/rightscreen.dart
+++ lib/screens/dashboard/edu_dashboard/student_portal/boards/rightscreen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -6,27 +7,82 @@ class Rightscreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView( 
-       scrollDirection: Axis.vertical, 
-       children: [
-        Container(
-          child:  Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ListView( 
+         scrollDirection: Axis.vertical, 
+         children: [
+          Container(
+            child:  Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+      
+                // Premium Card
+                Container(
+                  height: 260, 
+                  width: 380, 
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20), 
+                    color: Colors.black
+                  ),
+                  child:  Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
 
-              // Premium Card
-              Container(
-                height: 300, 
-                width: 300, 
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10), 
-                ),
-              )
-            ],
-          ),
-        )
-       ],
+                        // title 
+                        Text('Samyie', style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                        
+                        SizedBox(height: 20,), 
+                        Row(
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text('Go Premium', style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.w500),
+                                ), 
 
+                                SizedBox(height: 10,),
+                                Text('Explore 200+ Courses with \nPremium Membership', style: TextStyle(color: Colors.grey, fontSize: 12, fontWeight: FontWeight.w200),
+                                ), 
+                              ],
+                            ), 
+                            SizedBox(width: 30,), 
+                            Expanded(
+                              child: Container(
+                                width: 80, 
+                                height: 120,
+                                child: Image.asset('assets/dashboard/icons/model.png')))
+                          ],
+                        )
+, 
+SizedBox(height: 20,), 
+
+
+GestureDetector( 
+  onTap: () {}, 
+  child: Container(
+    height: 32, width: 120,
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(8),
+      color: Colors.yellowAccent
+    ),
+    child: Align(child: Text('Get Access')),
+  ),
+)
+
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )
+         ],
+      
+      ),
     );
   }
 }
