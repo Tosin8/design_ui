86fa0b8f286d7e8445ee886012bd5aed2b5f0522
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-06-04 17:45:37 +0100

    Update rightscreen.dart

--- lib/screens/dashboard/edu_dashboard/student_portal/boards/rightscreen.dart
+++ lib/screens/dashboard/edu_dashboard/student_portal/boards/rightscreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ui_design/screens/dashboard/edu_dashboard/student_portal/boards/middlescreen.dart';
 
 class Rightscreen extends StatelessWidget {
   const Rightscreen({super.key});
@@ -11,75 +12,88 @@ class Rightscreen extends StatelessWidget {
          scrollDirection: Axis.vertical, 
          children: [
           Container(
-            child:  Column(
+            child:  const Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
       
                 // Premium Card
-                Container(
-                  height: 260, 
-                  width: 380, 
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20), 
-                    color: Colors.black
-                  ),
-                  child:  Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-
-                        // title 
-                        const Text('Samyie', style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
-                        ),
-                        
-                        const SizedBox(height: 20,), 
-                        Row(
-                          children: [
-                            const Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text('Go Premium', style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.w500),
-                                ), 
-
-                                SizedBox(height: 10,),
-                                Text('Explore 200+ Courses with \nPremium Membership', style: TextStyle(color: Colors.grey, fontSize: 12, fontWeight: FontWeight.w200),
-                                ), 
-                              ],
-                            ), 
-                            const SizedBox(width: 30,), 
-                            Expanded(
-                              child: Container(
-                                width: 80, 
-                                height: 120,
-                                child: Image.asset('assets/dashboard/icons/model.png')))
-                          ],
-                        )
-, 
-const SizedBox(height: 20,), 
-
-
-GestureDetector( 
-  onTap: () {}, 
-  child: Container(
-    height: 32, width: 120,
-    decoration: BoxDecoration(
-      borderRadius: BorderRadius.circular(8),
-      color: Colors.yellowAccent
-    ),
-    child: const Align(child: Text('Get Access')),
-  ),
-)
-
-                      ],
-                    ),
-                  ),
-                )
+                PremiumCard(), 
+                SizedBox(height: 10,), 
+                titleHeader(title: 'Assignments',), 
               ],
             ),
           )
          ],
       
+      ),
+    );
+  }
+}
+
+class PremiumCard extends StatelessWidget {
+  const PremiumCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 260, 
+      width: 380, 
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20), 
+        color: Colors.black
+      ),
+      child:  Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+    
+            // title 
+            const Text('Samyie', style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            
+            const SizedBox(height: 20,), 
+            Row(
+              children: [
+                const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Go Premium', style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.w500),
+                    ), 
+    
+                    SizedBox(height: 10,),
+                    Text('Explore 200+ Courses with \nPremium Membership', style: TextStyle(color: Colors.grey, fontSize: 12, fontWeight: FontWeight.w200),
+                    ), 
+                  ],
+                ), 
+                const SizedBox(width: 30,), 
+                Expanded(
+                  child: Container(
+                    width: 80, 
+                    height: 120,
+                    child: Image.asset('assets/dashboard/icons/model.png')))
+              ],
+            )
+    , 
+    const SizedBox(height: 20,), 
+    
+    
+    GestureDetector( 
+      onTap: () {}, 
+      child: Container(
+        height: 32, width: 120,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: Colors.yellowAccent
+        ),
+        child: const Align(child: Text('Get Access')),
+      ),
+    )
+    
+          ],
+        ),
       ),
     );
   }
