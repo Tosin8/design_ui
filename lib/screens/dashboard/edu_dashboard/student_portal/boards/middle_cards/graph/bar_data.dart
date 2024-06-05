78d2bb3292dec24c58a5f6db04abbf67dc06f808
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-06-05 15:46:05 +0100

    Create bar_data.dart

---
+++ lib/screens/dashboard/edu_dashboard/student_portal/boards/middle_cards/graph/bar_data.dart
@@ -0,0 +1,9 @@
+class BarData {
+  final double sunActivity; 
+  final double monActivity; 
+  final double tueActivity; 
+  final double wedActivity; 
+  final double thurActivity; 
+  final double friActivity; 
+  final double satActivity; 
+}
