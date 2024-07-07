3eb16c8ace013237a18703bee1de3c859288b720
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-07-07 20:16:04 +0100

    Update moviebox.dart

--- lib/shots/moviebox.dart
+++ lib/shots/moviebox.dart
@@ -11,30 +11,35 @@ class _MovieboxState extends State<Moviebox> {
   @override
   Widget build(BuildContext context) {
     return Scaffold( 
-      backgroundColor: Colors.grey[300],
+      backgroundColor: Colors.black, 
       appBar: AppBar(
-        backgroundColor: Colors.grey[300],
-        title: const Text('Movie Box'),
+        backgroundColor: Colors.black, 
+        title: const Text('Movie Box', style: TextStyle(color: Colors.white),),
         centerTitle: true,
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: ListView(
           children:   const [
-             Text('Hi, Mikky', style: TextStyle(fontSize: 18),), 
+             Text('Hi, Mikky', style: TextStyle(fontSize: 18, color: Colors.white),), 
+             SizedBox(height: 10,), 
           TextField(
             textInputAction: TextInputAction.done,
             keyboardType: TextInputType.name,
   decoration: InputDecoration(
-    
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(10)),
+       ),
     filled: true,
-  //  labelText: 'Search',
+  // labelText: 'Search',
     prefixIcon: Icon(Icons.search, color: Colors.black),
     hintText: 'Search for movies',
     fillColor: Colors.white, 
   ),
 
              ), 
+             SizedBox(height: 10,), 
+             Text('Trending Movies', style: TextStyle(fontSize: 18, color: Colors.white),),
              
           ],
         ),
