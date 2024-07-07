ad96afa89ec990cc2242d32efa0e35eb0210ecb7
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-07-07 21:39:19 +0100

    Update moviebox.dart

--- lib/shots/moviebox.dart
+++ lib/shots/moviebox.dart
@@ -125,7 +125,15 @@ class _PlayMyVideoState extends State<PlayMyVideo> {
    ..initialize().then((value) => _controller.play());
  }
 
-
+String _videoDuration(Duration duration){
+  String twoDigits(int n) => n.toString().padLeft(2, '0'); 
+  final hours = twoDigits(duration.inHours); 
+  final minutes = twoDigits(duration.inMinutes.remainder(60)); 
+  final seconds = twoDigits(duration.inSeconds.remainder(60)); 
+  return [
+    if (duration.inHours > 0) hours, minutes, seconds
+  ].join(':'); 
+}
    @override
    void initState() {
      _playVideo(init: true); 
@@ -157,7 +165,7 @@ class _PlayMyVideoState extends State<PlayMyVideo> {
               children: [
 ValueListenableBuilder(
   valueListenable: _controller,
-   builder: (context, value, child) {
+   builder: (context, VideoPlayerValue value, child) {
   return Text(
     _videoDuration(value.position), 
     style: const TextStyle(color: Colors.white, fontSize: 20),);
