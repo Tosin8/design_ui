56c8c8f5437951ba9e8c9efead62fda9b985cc99
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-07-14 20:10:50 +0100

    Updating UI for splash Screen

--- lib/screens/grocery/grocery_splash.dart
+++ lib/screens/grocery/grocery_splash.dart
@@ -1,1 +1,20 @@
-g
+import 'package:flutter/material.dart';
+
+class GrocerySplash extends StatelessWidget {
+  const GrocerySplash({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            height: 400, 
+            width: double.infinity, 
+            child: Image.asset(''),
+          )
+        ],
+      ),
+    );
+  }
+}
