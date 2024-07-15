e322ae946944b14c954c7a551e17193749f7f8db
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-07-15 03:42:02 +0100

    Editing product model

--- lib/screens/grocery/model/product_model.dart
+++ lib/screens/grocery/model/product_model.dart
@@ -1,18 +1,55 @@
 class Product {
   final String title; 
   final String image;
+  final String price; 
+  final String description; 
 
-  Product({required this.title, required this.image}); 
+  Product({required this.price, required this.description, required this.title, required this.image}); 
 }
 
 List<Product> veggie_products = [
-  Product(title: 'Cabbage', image: 'assets/grocery/products/veggie/cabbage.png'),
-  Product(title: 'Broccoli', image: 'assets/grocery/products/veggie/broccoli.png'),
-  Product(title: 'Carrot', image: 'assets/grocery/products/veggie/carrot.png'),
-  Product(title: 'Potato', image: 'assets/grocery/products/veggie/potato.png'),
-  Product(title: 'Onion', image: 'assets/grocery/products/veggie/onion.png'),
-  Product(title: 'Tomato', image: 'assets/grocery/products/veggie/tomato.png'),
-  Product(title: 'Spinach', image: 'assets/grocery/products/veggie/spinach.png'),
-  Product(title: 'Cauliflower', image: 'assets/grocery/products/veggie/cauliflower.png'),
-  Product(title: 'Cucumber', image: 'assets/grocery/products/veggie/cucumber.png'),
+  Product(
+    title: 'Cabbage', 
+    image: 'assets/grocery/products/veggie/cabbage.png', 
+  description: 'Round, tightly packed head of leaves. Available in green, red, or savoy varieties. Crisp texture with a slightly peppery flavor when raw, becoming sweeter when cooked.' ,
+  price: '100/head', 
+  ),
+
+
+  Product(title: 'Broccoli', image: 'assets/grocery/products/veggie/broccoli.png', 
+  price: '2.49/lb', 
+  description: 'Tree-like structure with a thick, edible stem and green florets. Has a grassy, slightly bitter taste when raw, becoming milder when cooked'),
+
+
+  Product(title: 'Carrot', image: 'assets/grocery/products/veggie/carrot.png', 
+  price: '2.49/lb',
+  description: 'Round, tightly packed head of leaves. Available in green, red, or savoy varieties. Crisp texture with a slightly peppery flavor when raw, becoming sweeter when cooked.'),
+
+  Product(title: 'Potato', image: 'assets/grocery/products/veggie/potato.png', 
+  price: '2.49/lb',
+  description: 'Round, tightly packed head of leaves. Available in green, red, or savoy varieties. Crisp texture with a slightly peppery flavor when raw, becoming sweeter when cooked.'),
+
+
+  Product(title: 'Onion', image: 'assets/grocery/products/veggie/onion.png', 
+  price: '2.49/lb',
+  description: 'Layered bulb with papery outer skin. Available in yellow, red, and white varieties. Strong, pungent flavor that sweetens when cooked.'),
+
+
+  Product(title: 'Tomato', image: 'assets/grocery/products/veggie/tomato.png', 
+  price: '2.49/lb',
+  description: ' Juicy, round fruit with thin skin. Ranges from small cherry tomatoes to large beefsteak varieties. Slightly sweet, acidic flavor.'),
+
+  Product(title: 'Spinach', image: 'assets/grocery/products/veggie/spinach.png',
+  price: '2.99/bunch',
+  description: ' Dark green, broad leaves with a tender texture. Mild, slightly sweet flavor when young, becoming more bitter as it matures'
+  ),
+
+  Product(title: 'Cauliflower', image: 'assets/grocery/products/veggie/cauliflower.png', 
+  price: '2.79/head', 
+  description: 'Dense clusters of white florets surrounded by green leaves. Mild, slightly nutty flavor with a firm, crumbly texture.'),
+
+
+  Product(title: 'Cucumber', image: 'assets/grocery/products/veggie/cucumber.png', 
+  price: '2.49/lb',
+  description: ' Long, cylindrical fruit with green skin and pale flesh. High water content gives it a cool, crisp texture. Mild, refreshing flavor.'), 
 ]; 
--- lib/screens/grocery/model/veggie/veggie_productcard.dart
+++ lib/screens/grocery/model/veggie/veggie_productcard.dart
@@ -1,10 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:ui_design/screens/grocery/constants/app_info.dart';
+
+import '../product_model.dart';
 
 class ProductCard extends StatelessWidget {
-  const ProductCard({super.key});
+  const ProductCard({super.key, required this.product, required this.press});
 
+
+final Product product; 
+final VoidCallback press; 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GestureDetector( 
+      onTap: press,
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: defaultPadding), 
+        decoration: BoxDecoration(
+          color: Color(0xFFF7F7F7), 
+          borderRadius: BorderRadius.all(Radius.circular(defaultPadding * 1.25), 
+          ), 
+        ),
+        child: Column(
+          children: [
+            Hero(tag: product.title, 
+            child: Image.asset(product.image),
+            ), 
+            Text(
+product.title,  
+style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+            ), 
+            Text('Veggies', style: TextStyle(color: Colors.grey),), 
+            Row(
+              children: [
+                Text('\$${product.price}', style: TextStyle(color: primaryColor),), 
+                Spacer(), 
+                Icon(Iconsax.bag, color: primaryColor, size: 28,),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
