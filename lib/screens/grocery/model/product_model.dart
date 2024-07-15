f0dc1b1731eb06fdf111c7529b521f41a3d7d049
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-07-15 02:03:51 +0100

    adding veggies products

--- lib/screens/grocery/model/product_model.dart
+++ lib/screens/grocery/model/product_model.dart
@@ -4,3 +4,9 @@ class Product {
 
   Product({required this.title, required this.image}); 
 }
+
+List<Product> _products = [
+  Product(title: 'Apple', image: 'assets/grocery/1.jpg'),
+  Product(title: 'Banana', image: 'assets/grocery/2.jpg'),
+  Product(title: 'Orange', image: 'assets/grocery/3.jpg'),
+]; 
