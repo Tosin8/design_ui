1d9f074433991d677707d156b7bfb70d88240eee
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-07-15 02:22:53 +0100

    done adding products

--- lib/screens/grocery/model/product_model.dart
+++ lib/screens/grocery/model/product_model.dart
@@ -6,7 +6,13 @@ class Product {
 }
 
 List<Product> _products = [
-  Product(title: 'Apple', image: 'assets/grocery/1.jpg'),
-  Product(title: 'Banana', image: 'assets/grocery/2.jpg'),
-  Product(title: 'Orange', image: 'assets/grocery/3.jpg'),
+  Product(title: 'Cabbage', image: 'assets/grocery/products/veggie/cabbage.png'),
+  Product(title: 'Broccoli', image: 'assets/grocery/products/veggie/broccoli.png'),
+  Product(title: 'Carrot', image: 'assets/grocery/products/veggie/carrot.png'),
+  Product(title: 'Potato', image: 'assets/grocery/products/veggie/potato.png'),
+  Product(title: 'Onion', image: 'assets/grocery/products/veggie/onion.png'),
+  Product(title: 'Tomato', image: 'assets/grocery/products/veggie/tomato.png'),
+  Product(title: 'Spinach', image: 'assets/grocery/products/veggie/spinach.png'),
+  Product(title: 'Cauliflower', image: 'assets/grocery/products/veggie/cauliflower.png'),
+  Product(title: 'Cucumber', image: 'assets/grocery/products/veggie/cucumber.png'),
 ]; 
