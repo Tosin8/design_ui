43ce24dc6c871d6b6828216b65d26c2c99eb6cf0
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-07-15 05:29:48 +0100

    Update veggie_detail_screen.dart

---
+++ lib/screens/grocery/model/veggie/veggie_detail_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class VeggieDetailScreen extends StatefulWidget {
+  const VeggieDetailScreen({super.key});
+
+  @override
+  State<VeggieDetailScreen> createState() => _VeggieDetailScreenState();
+}
+
+class _VeggieDetailScreenState extends State<VeggieDetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
