900aa6ae9f48460fa8bb78300a03376663d255fd
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-07-17 19:13:05 +0100

    Create grocery2_home.dart

---
+++ lib/screens/grocery2/grocery2_home.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Grocery2Home extends StatelessWidget {
+  const Grocery2Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
