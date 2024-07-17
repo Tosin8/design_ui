0e898189ed0b447fd3b38d997bcad017aef1245e
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-07-17 22:07:39 +0100

    Update

--- lib/screens/grocery2/grocery2_home.dart
+++ lib/screens/grocery2/grocery2_home.dart
@@ -5,6 +5,8 @@ class Grocery2Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold( 
+      appBar: AppBar(title: const Text('Grocery 2')),
+    );
   }
 }
--- lib/screens/grocery2/grocery2_splash.dart
+++ lib/screens/grocery2/grocery2_splash.dart
@@ -8,9 +8,37 @@ class Grocery2Splash extends StatelessWidget {
     return SafeArea(
       child: Scaffold( 
         body: Container(
-          decoration: BoxDecoration(
+          height: double.infinity,
+          width: double.infinity,
+          decoration: const BoxDecoration(
             image: DecorationImage(image: AssetImage('assets/grocery2/splash_main.jpg'), fit: BoxFit.cover),
           ),
+          foregroundDecoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.black38, Colors.transparent,Colors.transparent], 
+              
+              begin: Alignment.bottomCenter, end: Alignment.topRight)),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+
+                  // title
+                  const Text('Right Taste', style: TextStyle(color: Colors.black, fontSize: 30, fontWeight: FontWeight.w500),),
+
+                  // subtitle 
+                  const Text('The Right Taste for Every Belly', style: TextStyle(color: Colors.black, fontSize: 14, fontWeight: FontWeight.w400),),
+
+                  const SizedBox(height: 100,),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      height: 50, width: 200,
+                      decoration: BoxDecoration(color: Colors.black, borderRadius: BorderRadius.circular(10)),
+                      child: const Align(child: Text('Get Started', style: TextStyle(color: Colors.white, fontSize: 18),)),
+                    ),
+                  )
+                ],
+              ),
         )
       ),
     );
