6827f3f02be41ecb43c9e77e9594624866d2ed0d
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-07-29 00:15:05 +0100

    Update search_home.dart

--- lib/screens/seachable_list/search_home.dart
+++ lib/screens/seachable_list/search_home.dart
@@ -91,7 +91,12 @@ username: '@ellianpol',
           )
         ),
         body: Container(
-          color: Colors.grey.shade900
+          color: Colors.grey.shade900, 
+          child: ListView.builder(
+            itemCount: _foundedUsers.length,
+            itemBuilder: (context , index) {
+              return userComponent(user: _foundedUsers[index]); 
+            }),
         )
       ),
     );
@@ -99,19 +104,60 @@ username: '@ellianpol',
 
   userComponent({required User user}) {
     return Container(
-      padding: const EdgeInsets.only(top: 10, bottom: 10), 
+      padding: const EdgeInsets.only(top: 10, bottom: 10, right: 10, left: 10), 
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          SizedBox(
-            width: 60, 
-            height: 60, 
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(50), 
-              child: Image.network(user.image),
-            ),
+          Row(
+            children: [
+              SizedBox(
+                width: 60, 
+                height: 60, 
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(50), 
+                  child: Image.network(user.image),
+                ),
+              ),
+           
+          const SizedBox(width: 10,), 
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(user.name, style: const TextStyle(color: Colors.white, fontWeight: FontWeight.w500),
+              ), 
+              const SizedBox(height: 5,), 
+              Text(user.username, style: TextStyle(color: Colors.grey[500]),
+              )
+            ],
           )
         ],
       ),
+      GestureDetector(
+        onTap: (){
+
+          setState(() {
+            user.isFollowedByMe = !user.isFollowedByMe; 
+          });
+        },
+        child: AnimatedContainer(
+          height: 35, 
+          width: 110,
+          duration: const Duration(
+            milliseconds: 300), 
+            decoration: BoxDecoration(
+              color: user.isFollowedByMe ? Colors.blue[700] : const Color(0x00ffffff), 
+              borderRadius: BorderRadius.circular(5), 
+              border: Border.all(color: user.isFollowedByMe ? Colors.transparent : Colors.grey.shade700)
+            ),
+            child: Center(
+              child: Text(user.isFollowedByMe ? 'Unfollow' : 'Follow', 
+              style: TextStyle(
+                color: user.isFollowedByMe ? Colors.white : Colors.grey[500]
+              ),)),
+        ),
+      )
+     ],
+          ), 
     ); 
   }
 }
