a78130ac96cbfef14a917e5a04964f180914473e
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-07-28 21:49:11 +0100

    Update search_home.dart

--- lib/screens/seachable_list/search_home.dart
+++ lib/screens/seachable_list/search_home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'model/user.dart';
+
 class SearchHome extends StatefulWidget {
   const SearchHome({super.key});
 
@@ -8,6 +10,44 @@ class SearchHome extends StatefulWidget {
 }
 
 class _SearchHomeState extends State<SearchHome> {
+final List<User> _users =[
+
+User(
+  name: 'Ellian Polacis', 
+username: '@ellianpol',
+ image: 'https://unsplash.com/photos/woman-in-white-turtleneck-sweater-DQjWG8NBGWg',
+  isFollowedByMe: false), 
+
+  User(
+  name: 'Ellian Polacis', 
+username: '@ellianpol',
+ image: 'https://unsplash.com/photos/a-man-looking-at-the-camera-bsNe8jk8tM0',
+  isFollowedByMe: false),
+   User(
+  name: 'Ellian Polacis', 
+username: '@ellianpol',
+ image: 'https://unsplash.com/photos/woman-in-white-turtleneck-sweater-DQjWG8NBGWg',
+  isFollowedByMe: false), User(
+  name: 'Ellian Polacis', 
+username: '@ellianpol',
+ image: 'https://unsplash.com/photos/woman-in-white-turtleneck-sweater-DQjWG8NBGWg',
+  isFollowedByMe: false), User(
+  name: 'Ellian Polacis', 
+username: '@ellianpol',
+ image: 'https://unsplash.com/photos/woman-in-white-turtleneck-sweater-DQjWG8NBGWg',
+  isFollowedByMe: false), User(
+  name: 'Ellian Polacis', 
+username: '@ellianpol',
+ image: 'https://unsplash.com/photos/woman-in-white-turtleneck-sweater-DQjWG8NBGWg',
+  isFollowedByMe: false), User(
+  name: 'Ellian Polacis', 
+username: '@ellianpol',
+ image: 'https://unsplash.com/photos/woman-in-white-turtleneck-sweater-DQjWG8NBGWg',
+  isFollowedByMe: false), 
+
+]; 
+final List<User> _foundedUsers = []; 
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
