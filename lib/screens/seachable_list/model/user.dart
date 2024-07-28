ffa381904f5b744449ec6521f9b51cf4d76d2507
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-07-28 21:40:00 +0100

    Create user.dart

---
+++ lib/screens/seachable_list/model/user.dart
@@ -0,0 +1,13 @@
+class User{
+  final String name; 
+  final String username; 
+  final String image; 
+  bool isFollowedByMe;
+
+  User({
+    required this.name,
+     required this.username, 
+     required this.image,
+     required this.isFollowedByMe}); 
+
+}
