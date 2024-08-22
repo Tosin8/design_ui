2dbb4b09af3e5236e12c035a0828766f0c111f4c
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-08-22 21:29:03 +0100

    Update travel_home.dart

--- lib/screens/travel_ui_transition/travel_home.dart
+++ lib/screens/travel_ui_transition/travel_home.dart
@@ -8,21 +8,50 @@ class TravelHome extends StatefulWidget {
 }
 
 class _TravelHomeState extends State<TravelHome> {
+  late PageController _pageController; 
+
+void _onScroll() {
+
+}
+  @override
+  void initState() {
+    _pageController = PageController(initialPage: 0)..addListener(_onScroll); 
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return  Scaffold( 
       body: PageView(
+        controller: _pageController,
         children: [
-          Container (
-            color: Colors.blue,
-          ), 
-Container (
-            color: Colors.green,
-          ), Container (
-            color: Colors.yellow,
-          ), 
+          Screen(image: 'assets/travel_ui/images/safari.jpg'), 
+            Screen(image: 'assets/travel_ui/images/victoria.jpg'),   
+            Screen(image: 'assets/travel_ui/images/wall.jpg'), 
+
                ],
       )
     );
+  }
+
+
+  Widget Screen({image}) {
+    return Container(
+      decoration:  BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(image), 
+        fit: BoxFit.cover), 
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.bottomRight, 
+            stops: const [0.3, 0.9],
+            colors: [Colors.black.withOpacity(.9), 
+          
+          Colors.black.withOpacity(0.2), 
+          ])
+        ),
+      )
+    ); 
   }
 }
