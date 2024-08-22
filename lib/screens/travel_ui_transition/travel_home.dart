ebc07d8ecdc181b3d7a8c32d8ec127a14b9ca728
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-08-22 21:08:25 +0100

    Update travel_home.dart

--- lib/screens/travel_ui_transition/travel_home.dart
+++ lib/screens/travel_ui_transition/travel_home.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
 
-class TravelHome extends StatelessWidget {
+class TravelHome extends StatefulWidget {
   const TravelHome({super.key});
 
   @override
+  State<TravelHome> createState() => _TravelHomeState();
+}
+
+class _TravelHomeState extends State<TravelHome> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold( 
-      
+    return  Scaffold( 
+      body: PageView(
+        children: [
+          Container (
+            color: Colors.blue,
+          ), 
+Container (
+            color: Colors.green,
+          ), Container (
+            color: Colors.yellow,
+          ), 
+               ],
+      )
     );
   }
 }
