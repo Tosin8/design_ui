d72c5efcfb89b8d880afa48e11e87d60c4b7d656
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-08-22 22:16:31 +0100

    Update fade_animation.dart

---
+++ lib/screens/travel_ui_transition/fade_animation.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+// using simple animation pub.dev
+class FadeAnimation extends StatelessWidget {
+  final double delay; 
+  final Widget child; 
+  const FadeAnimation({super.key, this.delay, this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
