b59f495583abe6875abe946b57dbbed2f259e8e3
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-09-12 20:11:36 +0100

    adding assets

--- lib/screens/otp/otp1.dart
+++ lib/screens/otp/otp1.dart
@@ -5,6 +5,12 @@ class OTP1 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold( 
+      body: Container(
+        child: const Column(
+          children: [],
+        ),
+      ),
+    );
   }
 }
