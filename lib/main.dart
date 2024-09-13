3b04e0e1570fc99dc7498f83cb7e0b86b71675fb
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-09-13 04:40:09 +0100

    updating

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ui_design/screens/otp/otp1.dart';
 import 'package:ui_design/screens/otp/otp2.dart';
+import 'package:ui_design/screens/pwd_validation/pwd_valid.dart';
 
 import 'screens/travel_onboard/swipe_home.dart';
 import 'screens/travel_ui_transition/travel_home.dart';
@@ -80,7 +81,7 @@ class MyApp extends StatelessWidget {
     //   tabletScaffold: TabletViewL(),
     //   desktopScaffold: DesktopView(),
     //   ), 
-home:  const otp2(), 
+home:  const PwdValid(), 
     ); 
   }
 }
--- lib/screens/pwd_validation/pwd_valid.dart
+++ lib/screens/pwd_validation/pwd_valid.dart
@@ -10,6 +10,33 @@ class PwdValid extends StatefulWidget {
 class _PwdValidState extends State<PwdValid> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold( 
+      body: Container(
+        width: double.infinity,
+        color: Colors.white, 
+        child:  Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0, vertical: 100),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text('Set a password', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold,height: 1.5),), 
+              const SizedBox(height: 10,), 
+               const Text('Kindly create a secure password \nand include the following criteria. ', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.grey),), 
+              const SizedBox(height: 20,),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10), 
+                    borderSide:  const BorderSide(color: Colors.black)
+                  ), 
+                  hintText: 'Password', 
+                  contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20), 
+                ),
+              ) 
+            ],
+          ),
+        ),
+      ), 
+    );
   }
 }
