67a8d8969f4ad734d443d2a9787e8254761e9a4b
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-09-15 21:53:27 +0100

    Update shop1.dart

--- lib/screens/restaurants/shop1/shop1.dart
+++ lib/screens/restaurants/shop1/shop1.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/shop_appbar.dart';
+
 class Shop1 extends StatefulWidget {
   const Shop1({super.key});
 
@@ -10,33 +12,10 @@ class Shop1 extends StatefulWidget {
 class _Shop1State extends State<Shop1> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold( 
+    return const Scaffold( 
       body: CustomScrollView(
         slivers: [
-          SliverAppBar(
-            expandedHeight: 200, 
-            flexibleSpace: FlexibleSpaceBar(
-               background: Container(
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('assets/food/all/17.jpg'
-                    
-                    ),
-                    fit: BoxFit.cover, 
-                ),
-                
-              ),
-              foregroundDecoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Colors.black, 
-                    Colors.black.withOpacity(0.6), 
-                  ]
-                  
-                  )
-              ),
-               ),),
-          )
+          ShopAppBar()
         ],
       )
     );
---
+++ lib/screens/restaurants/shop1/widgets/shop_appbar.dart
@@ -0,0 +1,57 @@
+
+
+import 'package:flutter/material.dart';
+
+class ShopAppBar extends StatelessWidget {
+  const ShopAppBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      expandedHeight: 200, 
+      flexibleSpace: FlexibleSpaceBar(
+         background: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/food/all/17.jpg'
+              
+              ),
+              fit: BoxFit.cover, 
+          ),
+          
+        ),
+        foregroundDecoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Colors.black.withOpacity(0.4), 
+              Colors.black.withOpacity(0.6), 
+            ], 
+            begin: Alignment.centerLeft, 
+            end: Alignment.topRight, 
+            )
+        ),
+         ),),
+         leading: const Padding(padding: EdgeInsets.only(left: 16), 
+         child: CircleAvatar( backgroundColor: Colors.white, 
+         child: Icon(Icons.arrow_back, color: Colors.black,),)
+         ), 
+         title: const Text('Belly Fix', 
+         style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        
+        ),
+        centerTitle: true,
+        actions: const [
+          Padding(padding: EdgeInsets.only(right: 16), 
+         child: CircleAvatar( backgroundColor: Colors.white, 
+         child: Icon(Icons.shopping_bag, color: Colors.black,),)
+         ),
+         Padding(
+           padding: EdgeInsets.only(right: 16.0),
+           child: CircleAvatar(backgroundColor: Colors.white, child: Icon(Icons.search, ),),
+         )
+        ],
+    );
+  }
+}
