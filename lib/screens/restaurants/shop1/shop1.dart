f1c2b2edc1303408a50827df4958f71fe37fc96a
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-09-15 21:40:24 +0100

    Update shop1.dart

--- lib/screens/restaurants/shop1/shop1.dart
+++ lib/screens/restaurants/shop1/shop1.dart
@@ -10,9 +10,34 @@ class Shop1 extends StatefulWidget {
 class _Shop1State extends State<Shop1> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold( 
+    return Scaffold( 
       body: CustomScrollView(
-        slivers: [],
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 200, 
+            flexibleSpace: FlexibleSpaceBar(
+               background: Container(
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/food/all/17.jpg'
+                    
+                    ),
+                    fit: BoxFit.cover, 
+                ),
+                
+              ),
+              foregroundDecoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Colors.black, 
+                    Colors.black.withOpacity(0.6), 
+                  ]
+                  
+                  )
+              ),
+               ),),
+          )
+        ],
       )
     );
   }
