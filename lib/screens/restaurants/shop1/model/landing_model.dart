8aa05216a2d4fc918ece2ba6658ba0793b5b5283
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-09-16 18:31:37 +0100

    Create landing_model.dart

---
+++ lib/screens/restaurants/shop1/model/landing_model.dart
@@ -0,0 +1,11 @@
+class Landing {
+  final String title; 
+  final String image; 
+  // navigation
+  final String route;
+  Landing({required this.title, required this.image, required this.route});
+}
+
+final List<Landing> demoLandings = [
+  
+]; 
