ab76162d65d51d3df4e09ac54840d901c429c97b
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-09-26 19:18:42 +0100

    updating

--- lib/screens/payment_api/payment_api.dart
+++ lib/screens/payment_api/payment_api.dart
@@ -16,6 +16,27 @@ class _PaymentApiState extends State<PaymentApi> {
   String publicKey = 'sk_test_4022a9a91a9fb0fece157832994253076af5e9df'; 
   final plugin = PaystackPlugin(); 
   String message = '';
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    plugin.initialize(publicKey: publicKey); 
+  }
+
+void makePayment()async{
+  int price = int.parse(amountController.text) * 100; 
+  Charge charge = Charge()
+  ..amount = price
+
+  ..reference = 'ref_${DateTime.now()}' 
+  ..email = emailController.text 
+  ..currency = 'NGN'; 
+
+  CheckoutResponse response = await plugin.checkout(context, charge: charge, 
+  method: CheckoutMethod.card); 
+}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold( 
@@ -81,7 +102,11 @@ class _PaymentApiState extends State<PaymentApi> {
             
                        ), 
                        const SizedBox(height: 20,), 
-                       ElevatedButton(onPressed: (){}, child: const Text('Make Payment Now'))
+                       ElevatedButton(onPressed: (){
+
+makePayment(); 
+
+                       }, child: const Text('Make Payment Now'))
           ],
         ),
       )
