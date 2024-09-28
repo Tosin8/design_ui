dc330913c365c703b84d2296569eb0a54147e692
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-09-28 17:39:21 +0100

    Create Quick Actions Home

---
+++ lib/screens/quick_actions/quick_actions_home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class QuickActionsHome extends StatefulWidget {
+  const QuickActionsHome({super.key});
+
+  @override
+  State<QuickActionsHome> createState() => _QuickActionsHomeState();
+}
+
+class _QuickActionsHomeState extends State<QuickActionsHome> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
