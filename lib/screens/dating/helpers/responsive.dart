1ec0ce398c8775a0f4a00eac13a4e5909a08d0fc
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-11-15 23:52:22 +0100

    Responsive Dart.

---
+++ lib/screens/dating/helpers/responsive.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ResponsiveWidget extends StatelessWidget {
+  const ResponsiveWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
