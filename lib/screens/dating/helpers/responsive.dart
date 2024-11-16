25b956fbf89d869cc2c70a6c9f6946ee5a760e74
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-11-16 23:17:37 +0100

    Update responsive.dart

--- lib/screens/dating/helpers/responsive.dart
+++ lib/screens/dating/helpers/responsive.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 
 class ResponsiveWidget extends StatelessWidget {
+
+  final Widget web; 
+  final Widget tablet; 
+  final Widget mobile; 
+
+  
   const ResponsiveWidget({super.key});
 
   @override
