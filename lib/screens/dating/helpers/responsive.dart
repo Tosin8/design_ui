69a4e7414f2d234b3288b15b82afdcc422cb3c8d
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-11-16 23:21:54 +0100

    Update responsive.dart

--- lib/screens/dating/helpers/responsive.dart
+++ lib/screens/dating/helpers/responsive.dart
@@ -1,13 +1,28 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 
+
+const int mobileWidth = 852; 
+  const int webWidth = 1200; 
 class ResponsiveWidget extends StatelessWidget {
 
   final Widget web; 
   final Widget tablet; 
   final Widget mobile; 
+  
 
   
-  const ResponsiveWidget({super.key});
+  const ResponsiveWidget({
+    Key? key,
+    required this.web,
+    required this.tablet,
+    required this.mobile,
+  }) : super(key: key);
+
+
+  static bool isMobile (BuildContext context) => MediaQuery.of(context).size.width < mobileWidth; // 600
+  static bool isTablet (BuildContext context) => MediaQuery.of(context).size.width >= 600 && MediaQuery.of(context).size.width < 1200; 
+  static bool isWeb (BuildContext context) => MediaQuery.of(context).size.width >= webWidth;
 
   @override
   Widget build(BuildContext context) {
